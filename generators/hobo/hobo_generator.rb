976ff037f4e288b6f07b998a1989539220269308
tom <tom@1a7aebd2-2222-0410-9f76-d2fcbb06e3f4>
2007-01-17 11:37:47 +0000

    git-svn-id: svn://hobocentral.net/hobo/branches/tom_sandbox@29 1a7aebd2-2222-0410-9f76-d2fcbb06e3f4

--- generators/hobo/hobo_generator.rb
+++ generators/hobo/hobo_generator.rb
@@ -1,6 +1,19 @@
 class HoboGenerator < Rails::Generator::Base
 
   def manifest
+    if options[:add_routes]
+      routes_path = File.join(RAILS_ROOT, "config/routes.rb")
+
+      route = "  Hobo.add_routes(map)\n"
+
+      route_src = File.read(routes_path)
+      return if route_src.include?(route)
+
+      head = "ActionController::Routing::Routes.draw do |map|"
+      route_src.sub!(head, head + "\n\n" + route)
+      File.open(routes_path, 'w') {|f| f.write(route_src) }
+    end
+
     record do |m|
       m.directory File.join("app/views/hobolib")
       m.directory File.join("app/views/hobolib/themes")
@@ -10,4 +23,15 @@ class HoboGenerator < Rails::Generator::Base
     end
   end
 
+  protected
+    def banner
+      "Usage: #{$0} generate [--add-routes]"
+    end
+
+    def add_options!(opt)
+      opt.separator ''
+      opt.separator 'Options:'
+      opt.on("--add-routes",
+             "Add Hobo routes to config/routes.rb") { |v| options[:add_routes] = v }
+    end
 end
