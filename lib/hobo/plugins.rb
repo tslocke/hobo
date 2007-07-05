c3377adc7b8a1d3a42a466e1d2dbb75587d66d57
james <james@1a7aebd2-2222-0410-9f76-d2fcbb06e3f4>
2007-07-05 12:50:58 +0000

    Improved plugin support
    git-svn-id: svn://hobocentral.net/hobo/branches/tom_sandbox@183 1a7aebd2-2222-0410-9f76-d2fcbb06e3f4

--- lib/hobo/plugins.rb
+++ lib/hobo/plugins.rb
@@ -1,17 +1,52 @@
-module Hobo::Plugins
-  class << self
-  
-    def make_hobo_model(name, &b)
-      make_class name, ActiveRecord::Base do
+module ::Hobo::Plugins
+  class HoboPlugin
+
+    def initialize(opt=nil)
+      @opt = opt || Hash.new
+      set_up_options(self.class::PLUGIN_DEFAULTS)
+
+      if @opt[:setup_using]
+        send @opt[:setup_using]
+      else
+        default
+      end
+    end
+
+    def set_up_options(*defaults)
+      @opt.reverse_merge!(*defaults)
+      make_variations(self.class::PLUGIN_SYMBOLS)
+    end
+
+    def make_variations(h)
+      h.each do |name|
+        if @opt[name]
+          # start with :product_category
+        
+          # add :product_categories
+          @opt[name.to_s.pluralize.to_sym] = @opt[name].to_s.pluralize.to_sym 
+
+          # :ProductCategory
+          @opt[name.to_s.camelize.to_sym] = @opt[name].to_s.camelize.to_sym 
+
+          # :ProductCategories
+          @opt[name.to_s.camelize.pluralize.to_sym] = @opt[name].to_s.camelize.pluralize.to_sym 
+        
+          # :ProductCategoriesController
+          @opt[(name.to_s.camelize.pluralize+'Controller').to_sym] =
+            (@opt[name].to_s.camelize.pluralize+'Controller').to_sym
+        end
+      end
+    end
+
+    def hobo_model(name, &b)
+      make_class @opt[name], ActiveRecord::Base do
         hobo_model
         class_eval &b if b
       end
-    end      
+    end
 
-    def make_resource_controller(name, &b)
-      controller_name = "#{name.to_s.pluralize}Controller".to_sym
-      
-      make_class controller_name, ApplicationController do
+    def resource_controller(name, &b)
+      make_class @opt[name], ApplicationController do
         hobo_model_controller
         class_eval &b if b
       end
@@ -23,7 +58,6 @@ module Hobo::Plugins
       c.class_eval(&b) if b
       c
     end
-  
-  end
     
+  end    
 end
