c5ae1f392212b9d73ffcef37a58df0410b09c8ea
james <james@1a7aebd2-2222-0410-9f76-d2fcbb06e3f4>
2007-08-16 14:11:42 +0000

    Fixed inplace editor update
    git-svn-id: svn://hobocentral.net/hobo/branches/tom_sandbox@319 1a7aebd2-2222-0410-9f76-d2fcbb06e3f4

--- lib/hobo/model_controller.rb
+++ lib/hobo/model_controller.rb
@@ -378,7 +378,7 @@ module Hobo
                 if changes.size == 1
                   # Decreasingly hacky support for the scriptaculous in-place-editor
                   new_val = Hobo::Dryml.render_tag(@template, "view",
-                                                   :with => @this, :field => changes.keys.first)
+                                                   :with => @this, :no_wrapper => true, :field => changes.keys.first)
                   hobo_ajax_response(@this, :new_field_value => new_val)
                 else
                   hobo_ajax_response(@this)
