3183e4bc05e25b35c17f189182b4987ef500f9a5
tom <tom@1a7aebd2-2222-0410-9f76-d2fcbb06e3f4>
2007-11-07 10:52:11 +0000

    adding new versions of rspec and rspec_on_rails
    git-svn-id: svn://hobocentral.net/hobo/branches/tom_sandbox@533 1a7aebd2-2222-0410-9f76-d2fcbb06e3f4

---
+++ hobo_spec/rails_root/vendor/plugins/rspec/lib/spec/runner/behaviour_runner.rb
@@ -0,0 +1,66 @@
+module Spec
+  module Runner
+    class BehaviourRunner
+      def initialize(options)
+        @options = options
+      end
+
+      def load_files(files)
+        # It's important that loading files (or choosing not to) stays the
+        # responsibility of the BehaviourRunner. Some implementations (like)
+        # the one using DRb may choose *not* to load files, but instead tell
+        # someone else to do it over the wire.
+        files.each do |file|
+          load file
+        end
+      end
+
+      def run
+        prepare
+        success = true
+        behaviours.each do |behaviour|
+          success = success & behaviour.suite.run
+        end
+        return success
+      ensure
+        finish
+      end
+
+      protected
+      def prepare
+        reporter.start(number_of_examples)
+        behaviours.reverse! if reverse
+        set_sequence_numbers
+      end
+
+      def finish
+        reporter.end
+        reporter.dump
+      end
+
+      def reporter
+        @options.reporter
+      end
+
+      def reverse
+        @options.reverse
+      end
+
+      # Sets the #number on each ExampleDefinition
+      def set_sequence_numbers
+        number = 0
+        behaviours.each do |behaviour|
+          number = behaviour.set_sequence_numbers(number)
+        end
+      end      
+
+      def behaviours
+        @options.behaviours
+      end
+
+      def number_of_examples
+        @options.number_of_examples
+      end
+    end
+  end
+end
---
+++ hobo_spec/rails_root/vendor/plugins/rspec/lib/spec/runner/formatter/specdoc_formatter.rb
@@ -0,0 +1,36 @@
+module Spec
+  module Runner
+    module Formatter
+      class SpecdocFormatter < BaseTextFormatter      
+        def add_behaviour(name)
+          @output.puts
+          @output.puts name
+          @output.flush
+        end
+      
+        def example_failed(example, counter, failure)
+          message = if failure.expectation_not_met?
+            "- #{example.description} (FAILED - #{counter})"
+          else
+            "- #{example.description} (ERROR - #{counter})"
+          end
+          
+          @output.puts(failure.expectation_not_met? ? red(message) : magenta(message))
+          @output.flush
+        end
+        
+        def example_passed(example)
+          message = "- #{example.description}"
+          @output.puts green(message)
+          @output.flush
+        end
+        
+        def example_pending(behaviour_name, example_name, message)
+          super
+          @output.puts yellow("- #{example_name} (PENDING: #{message})")
+          @output.flush
+        end
+      end
+    end
+  end
+end
