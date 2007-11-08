f6f78f893a34f736b4834764e27dd69b5c833c29
tom <tom@1a7aebd2-2222-0410-9f76-d2fcbb06e3f4>
2007-11-08 17:53:43 +0000

    adding builder script for testing the out-of-box Hobo app
    git-svn-id: svn://hobocentral.net/hobo/branches/tom_sandbox@539 1a7aebd2-2222-0410-9f76-d2fcbb06e3f4

---
+++ hobo_spec/build_app/build.rb
@@ -0,0 +1,132 @@
+#!/usr/bin/env ruby 
+
+# This script generates the POD demo from scratch. We can then run
+# some Selenium tests on it to ensure everything is in order
+
+require 'fileutils'
+
+APP_NAME = 'new_hobo_app'
+
+def sh(*s)
+  ok = system(s.join(' '))
+  exit(1) unless ok
+end
+
+def gen(s)
+  sh "ruby script/generate #{s}"
+end
+
+def edit(filename)
+  src = File.read(filename)
+  src = yield src
+  File.open(filename, 'w') {|f| f.write(src) }
+end
+
+FileUtils.rm_rf(APP_NAME)
+
+sh '../../bin/hobo --hobo-src ../../hobo', APP_NAME
+
+# sh 'cp -R new_hobo_app.bak new_hobo_app'
+
+Dir.chdir(APP_NAME) do
+  gen = "ruby #{File.join('script', 'generate')}"
+
+  gen "hobo_model advert title:string body:text"
+  gen "hobo_model_controller advert"
+  
+  gen "hobo_model category name:string"
+  gen "hobo_model_controller category"
+  
+  edit "app/models/user.rb" do |user|
+    user.sub(/  # --- Hobo Permissions --- #.*end\s*end/m, <<-END)
+  has_many :adverts
+
+  # --- Hobo Permissions --- #
+
+  def super_user?
+    login == 'admin'
+  end
+  
+  def creatable_by?(user)
+    user.guest?
+  end
+
+  def updatable_by?(user, new)
+    user == self
+  end
+
+  def deletable_by?(user)
+    false
+  end
+
+  def viewable_by?(user, field)
+    true
+  end
+
+end
+END
+  end
+  
+  edit "app/models/advert.rb" do |advert|
+    advert.sub(/  # --- Hobo Permissions --- #.*end\s*end/m, <<-END)
+  belongs_to :user
+  belongs_to :category
+
+  set_creator_attr :user
+
+  # --- Hobo Permissions --- #
+
+  def creatable_by?(user)
+    user == self.user
+  end
+
+  def updatable_by?(user, new)
+    user == self.user && same_fields?(new, :user)
+  end
+
+  def deletable_by?(user)
+    user == self.user
+  end
+
+  def viewable_by?(user, field)
+    true
+  end
+
+end
+END
+  end
+  
+  edit "app/models/category.rb" do |category|
+    category.sub(/  # --- Hobo Permissions --- #.*end\s*end/m, <<-END)
+  has_many :adverts
+
+  # --- Hobo Permissions --- #
+
+  def creatable_by?(user)
+    false
+  end
+
+  def updatable_by?(user, new)
+    false
+  end
+
+  def deletable_by?(user)
+    false
+  end
+
+  def viewable_by?(user, field)
+    true
+  end
+
+end
+END
+  end
+
+  sh "mysqladmin drop   new_hobo_app_development"
+  sh "mysqladmin create new_hobo_app_development"
+  
+  gen "hobo_migration"
+
+end
+
+
