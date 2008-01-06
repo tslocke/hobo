f9a154351f8867ffb6e3edc46d68a5aeafe10061
tom <tom@1a7aebd2-2222-0410-9f76-d2fcbb06e3f4>
2008-01-06 09:03:22 +0000

    front_controller generator: removed redundant --no-user option
    git-svn-id: svn://hobocentral.net/hobo/branches/sandbox@740 1a7aebd2-2222-0410-9f76-d2fcbb06e3f4

--- generators/hobo_front_controller/hobo_front_controller_generator.rb
+++ generators/hobo_front_controller/hobo_front_controller_generator.rb
@@ -1,6 +1,6 @@
 class HoboFrontControllerGenerator < Rails::Generator::NamedBase
 
-  default_options :no_user => false, :delete_index => false, :add_routes => false
+  default_options :delete_index => false, :add_routes => false
 
   def full_class_path
     class_path.blank? ? file_name : File.join(class_path, file_name)
@@ -34,8 +34,7 @@ class HoboFrontControllerGenerator < Rails::Generator::NamedBase
                  File.join('app/helpers', class_path, "#{file_name}_helper.rb"))
 
 
-      pages = options[:no_user] ? %w{index search} : %w{index search}
-      for page in pages
+      for page in %w{index search}
         m.template("#{page}.dryml", File.join('app/views', class_path, file_name, "#{page}.dryml"))
       end
     end
@@ -68,7 +67,7 @@ class HoboFrontControllerGenerator < Rails::Generator::NamedBase
 
   protected
     def banner
-      "Usage: #{$0} #{spec.name} <controller-name> [--add-routes] [--no-user] [--delete-index]"
+      "Usage: #{$0} #{spec.name} <controller-name> [--add-routes] [--delete-index]"
     end
 
     def add_options!(opt)
