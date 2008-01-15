f225cbd74c6b610b64372b774b2e113301ccdc3b
tom <tom@1a7aebd2-2222-0410-9f76-d2fcbb06e3f4>
2008-01-15 11:43:25 +0000

    Changes to rapid_recommendations model
    git-svn-id: svn://hobocentral.net/hobo/branches/sandbox@769 1a7aebd2-2222-0410-9f76-d2fcbb06e3f4

--- plugins/rapid_recommendations/models/recommendation.rb
+++ plugins/rapid_recommendations/models/recommendation.rb
@@ -1,15 +1,16 @@
 bundle_model :Recommendation do 
   
   fields do
+    comment _comment_format_, :primary_content => true
     timestamps
   end
 
-  belongs_to :user,    (_polymorphic_user_    ? { :polymorphic => true } : { :class_name => _RecommendationUser_ })
-  belongs_to :subject, (_polymorphic_subject_ ? { :polymorphic => true } : { :class_name => _RecommendationSubject_ })
+  belongs_to :author, (_polymorphic_user_    ? { :polymorphic => true } : { :class_name => _Author_ }), :creator => true
+  belongs_to :target, (_polymorphic_subject_ ? { :polymorphic => true } : { :class_name => _Target_ })
   
   def creatable_by?(user);         user == self.user; end
-  def updatable_by?(user, new);    false; end
+  def updatable_by?(user, new);    user == self.user; end
   def deletable_by?(deleter);      deleter == user; end
-  def viewable_by?(viewer, field); viewer  == user; end
+  def viewable_by?(viewer, field); true end
 
 end
