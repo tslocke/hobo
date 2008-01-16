cfbda87ea48a0614426bd799814dec921b6f136d
tom <tom@1a7aebd2-2222-0410-9f76-d2fcbb06e3f4>
2008-01-16 14:10:24 +0000

    Rapid-recommendations: better create permissions
    git-svn-id: svn://hobocentral.net/hobo/branches/sandbox@792 1a7aebd2-2222-0410-9f76-d2fcbb06e3f4

--- plugins/rapid_recommendations/models/recommendation.rb
+++ plugins/rapid_recommendations/models/recommendation.rb
@@ -7,13 +7,24 @@ bundle_model :Recommendation do
 
   belongs_to :author, :class_name => _Author_, :polymorphic => :optional, :creator => true
   belongs_to :target, :class_name => _Target_, :polymorphic => :optional
+
+  
+  def duplicate_recommendation?
+    # these methods are provided by the bundle; they return either the
+    # fkey or fkey + type-name pair, according to whether the
+    # association was set polymorphic or not.
+    fields = self.class.author_fields + self.class.target_fields
+    conditions = fields.map_hash {|f| send(f) }
+    self.class.find(:first, :conditions => conditions)
+  end
+  
   
   def creatable_by?(user)
-    user == author
+    user == author && !find_by_author_id && !duplicate_recommendation? && user != target.get_creator
   end
   
   def updatable_by?(user, new)
-    user == author || user.administrator?
+    user == author || user.administrator? && only_changed_fields?(new, :comment)
   end
   
   def deletable_by?(deleter)
