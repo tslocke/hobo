dc825912eab267de2ee09425f9f4f472d9179458
tom <tom@1a7aebd2-2222-0410-9f76-d2fcbb06e3f4>
2008-01-24 13:37:17 +0000

    Adding Hobo::EmailAddress#valid?
    git-svn-id: svn://hobocentral.net/hobo/branches/sandbox@831 1a7aebd2-2222-0410-9f76-d2fcbb06e3f4

--- lib/hobo/email_address.rb
+++ lib/hobo/email_address.rb
@@ -3,8 +3,11 @@ class Hobo::EmailAddress < String
   COLUMN_TYPE = :string
   
   def validate
-    "is not a valid email address" unless 
-      self.blank? || self =~ /^\s*([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\s*$/i
+    "is not a valid email address" unless valid?
+  end
+  
+  def valid?
+    self.blank? || self =~ /^\s*([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\s*$/i
   end
 
 end
