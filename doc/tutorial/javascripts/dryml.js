5d3b108b02666c72c46f5f334b5891816f958ea5
james <james@1a7aebd2-2222-0410-9f76-d2fcbb06e3f4>
2008-02-15 13:04:13 +0000

    Tutorial: restructuring and improvements to styling
    git-svn-id: svn://hobocentral.net/hobo/branches/sandbox@932 1a7aebd2-2222-0410-9f76-d2fcbb06e3f4

---
+++ doc/tutorial/javascripts/dryml.js
@@ -0,0 +1,19 @@
+CodeHighlighter.addStyle("html", {
+	comment : {
+		exp: /&lt;!\s*(--([^-]|[\r\n]|-[^-])*--\s*)&gt;/
+	},
+	tag : {
+		exp: /(&lt;\/?)([a-zA-Z]+\s?)/, 
+		replacement: "$1<span class=\"$0\">$2</span>"
+	},
+	string : {
+		exp  : /'[^']*'|"[^"]*"/
+	},
+	attribute : {
+		exp: /\b([a-zA-Z-:]+)(=)/, 
+		replacement: "<span class=\"$0\">$1</span>$2"
+	},
+	doctype : {
+		exp: /&lt;!DOCTYPE([^&]|&[^g]|&g[^t])*&gt;/
+	}
+});
