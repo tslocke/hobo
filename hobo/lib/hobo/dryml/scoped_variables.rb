a08e6d271971860019d05c346ddd127153603326
Tom Locke <tom@tomlocke.com>
2008-03-27 16:23:55 +0000

    DRYML: scoped variabled -- assigning to a variable now modifies it's value in a parent scope if it's present in one

--- hobo/lib/hobo/dryml/scoped_variables.rb
+++ hobo/lib/hobo/dryml/scoped_variables.rb
@@ -7,14 +7,12 @@ module Hobo::Dryml
     end
     
     def [](key)
-      @scopes.reverse_each do |s|
-        return s[key] if s.has_key?(key)
-      end 
-      nil
+     s = scope_with_key(key) and s[key]
     end
     
     def []=(key, val)
-      @scopes.last[key] = val
+      s = scope_with_key(key) || @scopes.last
+      s[key] = val
     end
     
     def new_scope
@@ -22,6 +20,13 @@ module Hobo::Dryml
       res = yield
       @scopes.pop
       res
+    end
+    
+    def scope_with_key(key)
+      @scopes.reverse_each do |s|
+        return s if s.has_key?(key)
+      end 
+      nil
     end
     
     def method_missing(name, *args)
