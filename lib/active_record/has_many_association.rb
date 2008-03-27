caf712fa78bba0643a4794a93388f34c0254ea64
Tom Locke <tom@tomlocke.com>
2008-03-27 10:13:27 +0000

    Source tidy

--- lib/active_record/has_many_association.rb
+++ lib/active_record/has_many_association.rb
@@ -28,13 +28,16 @@ module ActiveRecord::Associations
       proxy_reflection.klass
     end
     
+    
     def origin
       proxy_owner
     end
 
+    
     def origin_attribute
       proxy_reflection.association_name
     end
+    
     
     private
     
