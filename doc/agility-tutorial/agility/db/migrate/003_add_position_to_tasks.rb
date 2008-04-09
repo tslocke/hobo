1b7d2929638d3a4437fd923edbcfdf6c83f4e53e
Tom Locke <tom@tomlocke.com>
2008-04-09 11:45:33 +0100

    Remvoing and ignoring various files from agility app

--- doc/agility-tutorial/agility/db/migrate/003_add_position_to_tasks.rb
+++
@@ -1,9 +0,0 @@
-class AddPositionToTasks < ActiveRecord::Migration
-  def self.up
-    add_column :tasks, :position, :integer
-  end
-
-  def self.down
-    remove_column :tasks, :position
-  end
-end
