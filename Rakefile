bb5a46255d4fc6bb413a54e25b564fc0305320c5
Tom Locke <tom@tomlocke.com>
2008-04-12 11:15:30 +0100

    Changes to support usage as a gem

---
+++ Rakefile
@@ -0,0 +1,17 @@
+require 'rubygems'
+require 'hoe'
+
+$: << "./lib" 
+
+require 'hobofields'
+
+Hoe.new('hobofields', HoboFields::VERSION) do |p|
+  p.rubyforge_name = 'hobo'
+  p.author = 'Tom Locke'
+  p.email = 'tom@tomlocke.com'
+  p.url = "http://hobocentral.net/hobo-support"
+  p.summary = 'Rich field types and migration generator for Rails'
+  p.description = p.paragraphs_of('README.txt', 2..5).join("\n\n")
+  p.changes = "" # p.paragraphs_of('History.txt', 0..1).join("\n\n")
+end
+
--- lib/hobo_fields.rb
+++ lib/hobo_fields.rb
@@ -1,11 +1,15 @@
 require 'hobosupport'
 
+Dependencies.load_paths |= [ File.dirname(__FILE__) ] if defined?(Dependencies)
+
 module Hobo
   # Empty class to represent the boolean type.
   class Boolean; end
 end
 
 module HoboFields
+  
+  VERSION = "0.7.4"
   
   extend self
   
@@ -76,52 +80,60 @@ module HoboFields
     "HoboFields::#{class_name}".constantize if class_name
   end
   
-end
+  def enable
+    require "hobo_fields/enum_string"
+    require "hobo_fields/fields_declaration"
 
-require "hobo_fields/enum_string"
-
-# Add the fields do declaration to ActiveRecord::Base
-ActiveRecord::Base.send(:include, HoboFields::FieldsDeclaration)
-
-
-# Monkey patch ActiveRecord so that the attribute read & write methods
-# automatically wrap richly-typed fields.
-module ActiveRecord::AttributeMethods::ClassMethods
-
-  # Define an attribute reader method.  Cope with nil column.
-  def define_read_method(symbol, attr_name, column)
-    cast_code = column.type_cast_code('v') if column
-    access_code = cast_code ? "(v=@attributes['#{attr_name}']) && #{cast_code}" : "@attributes['#{attr_name}']"
-
-    unless attr_name.to_s == self.primary_key.to_s
-      access_code = access_code.insert(0, "missing_attribute('#{attr_name}', caller) " +
-                                       "unless @attributes.has_key?('#{attr_name}'); ")
-    end
-
-    # This is the Hobo hook - add a type wrapper around the field
-    # value if we have a special type defined
-    src = if connected? && (type_wrapper = try.attr_type(symbol)) &&
-              type_wrapper.is_a?(Class) && type_wrapper.not_in?(HoboFields::PLAIN_TYPES.values)
-            "val = begin; #{access_code}; end; " +
-              "if HoboFields.can_wrap?(val); self.class.attr_type(:#{attr_name}).new(val); else; val; end"
-          else
-            access_code
-          end
+    # Add the fields do declaration to ActiveRecord::Base
+    ActiveRecord::Base.send(:include, HoboFields::FieldsDeclaration)
     
-    evaluate_attribute_method(attr_name, 
-                              "def #{symbol}; @attributes_cache['#{attr_name}'] ||= begin; #{src}; end; end")
+    # Monkey patch ActiveRecord so that the attribute read & write methods
+    # automatically wrap richly-typed fields.
+    ActiveRecord::AttributeMethods::ClassMethods.class_eval do 
+      
+      # Define an attribute reader method.  Cope with nil column.
+      def define_read_method(symbol, attr_name, column)
+        cast_code = column.type_cast_code('v') if column
+        access_code = cast_code ? "(v=@attributes['#{attr_name}']) && #{cast_code}" : "@attributes['#{attr_name}']"
+
+        unless attr_name.to_s == self.primary_key.to_s
+          access_code = access_code.insert(0, "missing_attribute('#{attr_name}', caller) " +
+                                           "unless @attributes.has_key?('#{attr_name}'); ")
+        end
+
+        # This is the Hobo hook - add a type wrapper around the field
+        # value if we have a special type defined
+        src = if connected? && (type_wrapper = try.attr_type(symbol)) &&
+                  type_wrapper.is_a?(Class) && type_wrapper.not_in?(HoboFields::PLAIN_TYPES.values)
+                "val = begin; #{access_code}; end; " +
+                  "if HoboFields.can_wrap?(val); self.class.attr_type(:#{attr_name}).new(val); else; val; end"
+              else
+                access_code
+              end
+        
+        evaluate_attribute_method(attr_name, 
+                                  "def #{symbol}; @attributes_cache['#{attr_name}'] ||= begin; #{src}; end; end")
+      end
+      
+      def define_write_method(attr_name)
+        src = if connected? && (type_wrapper = try.attr_type(attr_name)) &&
+                  type_wrapper.is_a?(Class) && type_wrapper.not_in?(HoboFields::PLAIN_TYPES.values)
+                "begin; wrapper_type = self.class.attr_type(:#{attr_name}); " +
+                  "if !val.is_a?(wrapper_type) && HoboFields.can_wrap?(val); wrapper_type.new(val); else; val; end; end"
+              else
+                "val"
+              end
+        evaluate_attribute_method(attr_name,
+                                  "def #{attr_name}=(val); write_attribute('#{attr_name}', #{src});end", "#{attr_name}=")
+        
+      end
+
+    end
+    
+    
   end
   
-  def define_write_method(attr_name)
-    src = if connected? && (type_wrapper = try.attr_type(attr_name)) &&
-              type_wrapper.is_a?(Class) && type_wrapper.not_in?(HoboFields::PLAIN_TYPES.values)
-            "begin; wrapper_type = self.class.attr_type(:#{attr_name}); " +
-              "if !val.is_a?(wrapper_type) && HoboFields.can_wrap?(val); wrapper_type.new(val); else; val; end; end"
-          else
-            "val"
-          end
-    evaluate_attribute_method(attr_name, "def #{attr_name}=(val); write_attribute('#{attr_name}', #{src});end", "#{attr_name}=")
-    
-  end
-
 end
+
+
+HoboFields.enable if defined? ActiveRecord
