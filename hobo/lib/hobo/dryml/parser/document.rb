ca43b408086db3ec9d7c3104e83eb480b9d9b6cc
Tom Locke <tom@tomlocke.com>
2008-04-15 17:20:29 +0100

    Refactoring DRYML parsing to make it easier to parse DRYML in other contexts

--- hobo/lib/hobo/dryml/parser/document.rb
+++ hobo/lib/hobo/dryml/parser/document.rb
@@ -2,20 +2,47 @@ module Hobo::Dryml::Parser
   
   class Document < REXML::Document        
     
-    attr_accessor :default_attribute_value
-    
-    def initialize(source=nil, context={})
-      super(nil, context)
+    def initialize(source, path)
+      super(nil)
+      
+      # Replace <%...%> scriptlets with xml-safe references into a hash of scriptlets
+      @scriptlets = {}
+      source = source.gsub(/<%(.*?)%>/m) do
+        _, scriptlet = *Regexp.last_match
+        id = @scriptlets.size + 1
+        @scriptlets[id] = scriptlet
+        newlines = "\n" * scriptlet.count("\n")
+        "[![DRYML-ERB#{id}#{newlines}]!]"
+      end
+      
+      
+      @reference_src = "<dryml_page>" + source + "</dryml_page>"
+      rex_src = Hobo::Dryml::Parser::Source.new(@reference_src)
+        
       @elements = Hobo::Dryml::Parser::Elements.new(self)
-      if source.kind_of? Document
-        @context = source.context
-        super source
-      else
-        build(  source )
-      end 
+      build(rex_src)
+      
+    rescue REXML::ParseException => e
+      raise DrymlSyntaxError, "File: #{path}\n#{e}"
     end
     
+    
+    def element_line_num(el)
+      offset = el.source_offset
+      @reference_src[0..offset].count("\n") + 1
+    end
+    
+    
+    def default_attribute_value
+      "&true"
+    end
+    
+    
+    def restore_erb_scriptlets(src)
+      src.gsub(/\[!\[HOBO-ERB(\d+)\s*\]!\]/m) {|s| "<%#{@scriptlets[$1.to_i]}%>" }
+    end
 
+    
     private
     def build( source )
       Hobo::Dryml::Parser::TreeParser.new( source, self ).parse
