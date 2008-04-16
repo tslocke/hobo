c854dfa87261f457fdeba87c000cabcaead38a93
Tom Locke <tom@tomlocke.com>
2008-04-16 12:42:56 +0100

    Fixing conflict in model_router (when did that sneak in eh?)

--- hobo/lib/hobo/model_router.rb
+++ hobo/lib/hobo/model_router.rb
@@ -207,18 +207,6 @@ module Hobo
       linkable_route("reorder_#{plural}", "#{plural}/reorder", 'reorder', :conditions => { :method => :post })
     end
     
-<<<<<<< HEAD:hobo/lib/hobo/model_router.rb
-=======
-    
-    def lifecycle_routes
-      model::Lifecycle.creator_names.each do |creator|
-        linkable_route("#{singular}_#{creator}", "#{plural}/#{creator}", creator, :conditions => { :method => :post })
-      end
-      model::Lifecycle.transition_names do |transition|
-        linkable_route("#{singular}_#{transition}", "#{plural}/:id/#{transition}", transition, :conditions => { :method => :put })
-      end
-    end
->>>>>>> d7a9e38... Work on auto-completers:hobo/lib/hobo/model_router.rb
         
     def user_routes
       prefix = plural == "users" ? "" : "#{singular}_"
