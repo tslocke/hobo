f4c0708feea1f3eb06895f524f718588ffdfb90d
Tom Locke <tom@tomlocke.com>
2008-04-19 14:58:32 +0100

    Fix to :scope => :myscope on has-many-through associations

--- hobo/lib/hobo/scopes/association_proxy_extensions.rb
+++ hobo/lib/hobo/scopes/association_proxy_extensions.rb
@@ -4,27 +4,47 @@ module Hobo
   
   module Scopes
     
-    module AssociationProxyExtensions
-      
-      def self.included(base)
-        base.class_eval do 
-          alias_method_chain :conditions, :hobo_scopes
-        end
-      end
+    AssociationProxyExtensions = classy_module do
       
       def conditions_with_hobo_scopes
         scope_conditions = if (scope_name = @reflection.options[:scope])
                              target_class = @reflection.klass
                              target_class.send(scope_name).scope(:find)[:conditions]
                            end
-        if scope_conditions && conditions_without_hobo_scopes
+        unscoped_conditions = conditions_without_hobo_scopes
+        if scope_conditions && unscoped_conditions
           "(#{sanitize_sql conditions_without_hobo_scopes}) AND (#{sanitize_sql scope_conditions})"
         elsif scope_conditions
           sanitize_sql scope_conditions
         else
-          conditions_without_hobo_scopes
+          unscoped_conditions
         end
       end
+
+      alias_method_chain :conditions, :hobo_scopes
+      
+    end
+
+    # Horrible repitition, but you know what, sometimes you just do.
+    
+    HasManyThroughAssociationExtensions = classy_module do
+      
+      def sql_conditions_with_hobo_scopes
+        scope_conditions = if (scope_name = @reflection.options[:scope])
+                             target_class = @reflection.klass
+                             target_class.send(scope_name).scope(:find)[:conditions]
+                           end
+        unscoped_conditions = sql_conditions_without_hobo_scopes
+        if scope_conditions && unscoped_conditions
+          "(#{sanitize_sql conditions_without_hobo_scopes}) AND (#{sanitize_sql scope_conditions})"
+        elsif scope_conditions
+          sanitize_sql scope_conditions
+        else
+          unscoped_conditions
+        end
+      end
+
+      alias_method_chain :sql_conditions, :hobo_scopes
       
     end
       
