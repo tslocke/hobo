c3ed71de9e6b58a80d65d0f96ee8a515a4dee71e
Tom Locke <tom@tomlocke.com>
2008-05-23 12:16:03 +0100

    Fix to hobo_rapid generator

--- hobo/rails_generators/hobo_rapid/hobo_rapid_generator.rb
+++ hobo/rails_generators/hobo_rapid/hobo_rapid_generator.rb
@@ -13,7 +13,7 @@ class HoboRapidGenerator < Hobo::Generator
       m.file "hobo-rapid.js",      "public/javascripts/hobo-rapid.js"
       m.file "lowpro.js",          "public/javascripts/lowpro.js"
       m.file "IE7.js",             "public/javascripts/IE7.js"
-      m.file "nicEdit.js",         "public/javascripts/nicEdit.js"
+      m.file "nicedit.js",         "public/javascripts/nicedit.js"
       m.file "nicEditorIcons.gif", "public/images/nicEditorIcons.gif"
       m.file "reset.css",          "public/stylesheets/reset.css"
       m.file "hobo-rapid.css",     "public/stylesheets/hobo-rapid.css"
