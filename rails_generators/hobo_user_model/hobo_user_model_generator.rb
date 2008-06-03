81538074d689f1d02f26f15f794109b9d3d22c2e
Tom Locke <tom@tomlocke.com>
2008-06-02 23:08:00 -0400

    Fixes to forgotten password stuff in hobo_user_model generator

--- rails_generators/hobo_user_model/hobo_user_model_generator.rb
+++ rails_generators/hobo_user_model/hobo_user_model_generator.rb
@@ -9,13 +9,16 @@ class HoboUserModelGenerator < Rails::Generator::NamedBase
       m.directory File.join('app/models', class_path)
       m.directory File.join('test/unit', class_path)
       m.directory File.join('test/fixtures', class_path)
+      mailer_dir = File.join("app/views", class_path[0..-2], "#{file_name.singularize}_mailer")
+      m.directory mailer_dir
 
       # Model class, unit test, and fixtures.
-      m.template 'model.rb',            File.join('app/models', class_path, "#{file_name}.rb")
-      m.template 'mailer.erb',          File.join('app/models', class_path, "#{file_name}_mailer.rb")
-      m.template 'forgot_password.erb', File.join("app/views/#{file_name}", class_path, "forgot_password.erb")
-      m.template 'unit_test.rb',        File.join('test/unit', class_path, "#{file_name}_test.rb")
+      m.template 'model.rb',            File.join('app/models',    class_path, "#{file_name}.rb")
+      m.template 'unit_test.rb',        File.join('test/unit',     class_path, "#{file_name}_test.rb")
       m.template 'fixtures.yml',        File.join('test/fixtures', class_path, "#{table_name}.yml")
+
+      m.template 'mailer.erb',          File.join('app/models', class_path, "#{file_name}_mailer.rb")
+      m.template 'forgot_password.erb', File.join(mailer_dir, "forgot_password.erb")
     end
   end
 
