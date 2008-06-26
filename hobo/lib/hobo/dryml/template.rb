507b5de07a11059ec778d16a8fecde783cbdb0bc
Tom Locke <tom@tomlocke.com>
2008-06-26 11:38:29 +0100

    DRYML - removed incorrect leading '/' on template paths

--- hobo/lib/hobo/dryml/template.rb
+++ hobo/lib/hobo/dryml/template.rb
@@ -35,7 +35,7 @@ module Hobo::Dryml
     def initialize(src, environment, template_path, bundle=nil)
       @src = src
       @environment = environment # a class or a module
-      @template_path = template_path.sub(/^#{Regexp.escape(RAILS_ROOT)}/, "")
+      @template_path = template_path.sub(/^#{Regexp.escape(RAILS_ROOT)}/, "").sub(%r(^/), '')
       @bundle = bundle
 
       @builder = Template.build_cache[@template_path] || DRYMLBuilder.new(self)
