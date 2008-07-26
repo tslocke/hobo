9c6c1b85e4dabbe3c645aea66787f963366c5d63
Tom Locke <tom@tomlocke.com>
2008-07-26 13:39:18 +0100

    User controller -- fix #forgot_password

--- hobo/lib/hobo/user_controller.rb
+++ hobo/lib/hobo/user_controller.rb
@@ -90,7 +90,7 @@ module Hobo
         user = model.find_by_email_address(params[:email_address])
         if user && (!block_given? || yield(user))
           Hobo::Controller.request_host = request.host_with_port
-          Hobo::Controller.app_name = render_tag(:app_name)
+          Hobo::Controller.app_name = call_tag(:app_name)
           user.lifecycle.request_password_reset(:nobody)
         end
         render_tag :forgot_password_email_sent_page
