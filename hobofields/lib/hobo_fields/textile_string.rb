58fe9b0e82c121d4e188afea92af14a389746d37
Tom Locke <tom@tomlocke.com>
2008-08-19 12:56:52 +0100

    HoboFields -- removed RedCloth monkey-patch. Not needed (and broken) in RedCloth 4

--- hobofields/lib/hobo_fields/textile_string.rb
+++ hobofields/lib/hobo_fields/textile_string.rb
@@ -18,12 +18,3 @@ module HoboFields
   end
 
 end
-
-class RedCloth
-  # Patch for RedCloth.  Fixed in RedCloth r128 but _why hasn't released it yet.
-  # http://code.whytheluckystiff.net/redcloth/changeset/128
-  def hard_break( text )
-    text.gsub!( /(.)\n(?!\n|\Z| *([#*=]+(\s|$)|[{|]))/, "\\1<br />" ) if hard_breaks && RedCloth::VERSION == "3.0.4"
-  end
-end
-
