b551561498ce8c7d9d6563a3fa6f4ead12c0c7fe
Tom Locke <tom@tomlocke.com>
2008-10-01 17:49:50 +0100

    Hobo models -- refactoring MassAssignment so that helper method params_hash_to_array is available to models

--- lib/hobo/mass_assignment.rb
+++ lib/hobo/mass_assignment.rb
@@ -4,6 +4,9 @@ module Hobo
     
     include IncludeInSave
     
+    
+    # --- has_many mass assignment support --- #
+    
     def self.has_many_with_mass_assignment(name, options={}, &block)
       accessible = options.delete(:accessible)
       has_many_without_mass_assignment(name, options, &block)
@@ -12,22 +15,17 @@ module Hobo
         class_eval %{
           def #{name}_with_mass_assignment=(array_or_hash)
             self.#{name}_without_mass_assignment = prepare_has_many_assignment(:#{name}, array_or_hash)
-          end}, __FILE__
+          end}, __FILE__, __LINE__ - 3
         alias_method_chain :"#{name}=", :mass_assignment
       end
     end
     metaclass.alias_method_chain :has_many, :mass_assignment
-
-
+    
+    
     def prepare_has_many_assignment(association_name, array_or_hash)
       association = send(association_name)
 
-      array = if array_or_hash.is_a?(Hash)
-                array = array_or_hash.get(*array_or_hash.keys.sort_by(&:to_i))
-              else
-                array_or_hash
-              end
-
+      array = params_hash_to_array(array_or_hash)
       array.map do |record_or_hash|
         if record_or_hash.is_a?(Hash)
           hash = record_or_hash
@@ -52,6 +50,12 @@ module Hobo
 
     end      
     
-  end
-  
+    def params_hash_to_array(array_or_hash)
+      if array_or_hash.is_a?(Hash)
+        array = array_or_hash.get(*array_or_hash.keys.sort_by(&:to_i))
+      else
+        array_or_hash
+      end
+    end
+    
 end
