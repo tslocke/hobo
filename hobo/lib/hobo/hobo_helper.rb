4b84826d7e352267297a6d8a812c7a74029a5618
Tom Locke <tom@tomlocke.com>
2008-10-16 15:43:36 +0100

    Made object_url handle a scoped class (gives the index url, e.g. /posts)

--- hobo/lib/hobo/hobo_helper.rb
+++ hobo/lib/hobo/hobo_helper.rb
@@ -58,7 +58,7 @@ module Hobo
       options[:subsite] ||= self.subsite
       subsite, method = options.get :subsite, :method
 
-      if obj.respond_to?(:member_class)
+      if obj.respond_to?(:member_class) && obj.respond_to?(:origin)
         # Asking for URL of a collection, e.g. category/1/adverts or category/1/adverts/new
         
         refl = obj.origin.class.reverse_reflection(obj.origin_attribute)
@@ -90,7 +90,13 @@ module Hobo
           obj = obj.class
         end
         
-        klass = obj.is_a?(Class) ? obj : obj.class
+        klass = if obj.is_a?(Class)
+                  obj
+                elsif obj.respond_to?(:member_class)
+                  obj.member_class # We get here if we're passed a scoped class
+                else
+                  obj.class
+                end
       end
 
       if Hobo::ModelRouter.linkable?(klass, action, options)
