38f3c7081b1072895a8a7999bb89077e68bad302
Tom Locke <tom@tomlocke.com>
2008-10-24 10:52:04 +0100

    HoboSupport - re-org of the file layout

---
+++ hobosupport/lib/hobo_support.rb
@@ -0,0 +1,44 @@
+module HoboSupport
+
+  VERSION = "0.8.3"
+
+end
+
+
+# Some teeny fixes too diminutive to go elsewhere
+
+class Object
+
+  # Often nice to ask e.g. some_object.is_a?(Symbol, String)
+  alias_method :is_a_without_multiple_args?, :is_a?
+  def is_a?(*args)
+    args.any? {|a| is_a_without_multiple_args?(a) }
+  end
+
+  # This is in ActiveSupport but only in edge Rails.
+  def present?
+    !blank?
+  end
+
+end
+
+
+class String
+
+  # Return the constant that this string refers to, or nil if ActiveSupport cannot load such a
+  # constant. This is much safer than `rescue NameError`, as that will mask genuine NameErrors
+  # that have been made in the code being loaded (#safe_constantize will not)
+  def safe_constantize
+    Object.class_eval self
+  rescue NameError => e
+    # Unforunately we have to rely on the error message to figure out which constant was missing.
+    # NameError has a #name method but it is always nil
+    if e.message !~ /\b#{self}$/
+      # oops - some other name error
+      raise
+    else
+      nil
+    end
+  end
+
+end
--- hobosupport/lib/hobosupport.rb
+++ hobosupport/lib/hobosupport.rb
@@ -1,57 +1,13 @@
 require 'activesupport'
 
-require "hobosupport/fixes"
-require 'hobosupport/blankslate'
-require 'hobosupport/methodcall'
-require 'hobosupport/methodphitamine'
-require 'hobosupport/metaid'
-require 'hobosupport/implies'
-require 'hobosupport/enumerable'
-require 'hobosupport/array'
-require 'hobosupport/hash'
-require 'hobosupport/module'
-
-module HoboSupport
-
-  VERSION = "0.8.3"
-
-end
-
-
-# Some teeny fixes too diminutive to go elsewhere
-
-class Object
-
-  # Often nice to ask e.g. some_object.is_a?(Symbol, String)
-  alias_method :is_a_without_multiple_args?, :is_a?
-  def is_a?(*args)
-    args.any? {|a| is_a_without_multiple_args?(a) }
-  end
-
-  # This is in ActiveSupport but only in edge Rails.
-  def present?
-    !blank?
-  end
-
-end
-
-
-class String
-
-  # Return the constant that this string refers to, or nil if ActiveSupport cannot load such a
-  # constant. This is much safer than `rescue NameError`, as that will mask genuine NameErrors
-  # that have been made in the code being loaded (#safe_constantize will not)
-  def safe_constantize
-    Object.class_eval self
-  rescue NameError => e
-    # Unforunately we have to rely on the error message to figure out which constant was missing.
-    # NameError has a #name method but it is always nil
-    if e.message !~ /\b#{self}$/
-      # oops - some other name error
-      raise
-    else
-      nil
-    end
-  end
-
-end
+require 'hobo_support'
+require "hobo_support/fixes"
+require 'hobo_support/blankslate'
+require 'hobo_support/methodcall'
+require 'hobo_support/methodphitamine'
+require 'hobo_support/metaid'
+require 'hobo_support/implies'
+require 'hobo_support/enumerable'
+require 'hobo_support/array'
+require 'hobo_support/hash'
+require 'hobo_support/module'
