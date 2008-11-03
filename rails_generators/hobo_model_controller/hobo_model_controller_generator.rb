e6c8cb43c522ed85eeada99f6852da77ccc42fad
Tom Locke <tom@tomlocke.com>
2008-11-03 16:27:53 +0000

    model_controller_generator -- adding support for generating subsite controllers

--- rails_generators/hobo_model_controller/hobo_model_controller_generator.rb
+++ rails_generators/hobo_model_controller/hobo_model_controller_generator.rb
@@ -4,11 +4,20 @@ class HoboModelControllerGenerator < Rails::Generator::NamedBase
     args[0] = args[0].pluralize
     super(args, options)
   end
+  
+  attr_reader :subsite
 
   def manifest
     record do |m|
       # Check for class naming collisions.
       m.class_collisions class_path, "#{class_name}Controller", "#{class_name}ControllerTest", "#{class_name}Helper"
+      
+      if class_path.length == 1 and
+        subsite = class_path.first and
+        File.exist?(File.join('app/controllers', class_path, "#{subsite}_site_controller.rb"))
+        
+        @subsite = subsite.camelize
+      end
 
       # Controller, helper, views, and test directories.
       m.directory File.join('app/controllers', class_path)
