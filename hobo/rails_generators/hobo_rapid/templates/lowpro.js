63030e44823fd615d6276e1640d6d72ee713064c
Tom Locke <tom@tomlocke.com>
2008-11-07 14:03:28 +0000

    Try again -- hacking lowpro to be friendlier to Prototpye custom events (the event names have a ':' in them)

--- hobo/rails_generators/hobo_rapid/templates/lowpro.js
+++ hobo/rails_generators/hobo_rapid/templates/lowpro.js
@@ -2,7 +2,7 @@ LowPro = {};
 LowPro.Version = '0.5';
 LowPro.CompatibleWithPrototype = '1.6';
 
-if (Prototype.Version.indexOf(LowPro.CompatibleWithPrototype) != 0 && window.console && window.console.warn)
+if (Prototype.Version.indexOf(LowPro.CompatibleWithPrototype) != 0 && console && console.warn)
   console.warn("This version of Low Pro is tested with Prototype " + LowPro.CompatibleWithPrototype + 
                   " it may not work as expected with this version (" + Prototype.Version + ")");
 
@@ -15,19 +15,19 @@ DOM = {};
 // DOMBuilder for prototype
 DOM.Builder = {
 	tagFunc : function(tag) {
-    return function() {
-     var attrs, children;
-     if (arguments.length>0) {
-       if (arguments[0].constructor == Object) {
-         attrs = arguments[0];
-         children = Array.prototype.slice.call(arguments, 1);
-       } else {
-         children = arguments;
-       };
-       children = $A(children).flatten()
-     }
-     return DOM.Builder.create(tag, attrs, children);
-    };
+	  return function() {
+	    var attrs, children; 
+	    if (arguments.length>0) { 
+	      if (arguments[0].nodeName || 
+	        typeof arguments[0] == "string") 
+	        children = arguments; 
+	      else { 
+	        attrs = arguments[0]; 
+	        children = Array.prototype.slice.call(arguments, 1); 
+	      };
+	    }
+	    return DOM.Builder.create(tag, attrs, children);
+	  };
   },
 	create : function(tag, attrs, children) {
 		attrs = attrs || {}; children = children || []; tag = tag.toLowerCase();
@@ -119,7 +119,7 @@ Object.extend(Event.addBehavior, {
       var observer = rules[selector];
       var sels = selector.split(',');
       sels.each(function(sel) {
-        var parts = sel.split(/:(?=[a-z:]+$)/), css = parts[0], event = parts[1];
+        var match = sel.match(/^([^:]*)(?::(.*)$)?/), css = match[1], event = match[2];
         $$(css).each(function(element) {
           if (event) {
             observer = Event.addBehavior._wrapObserver(observer);
