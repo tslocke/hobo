2e6b70887e486e44a54835bd268bdffd6ddf20ef
Tom Locke <tom@tomlocke.com>
2008-12-03 12:12:19 +0000

    Permissions - edit permission on associations now takes into account :accessible => true metadata (only accessible associations are editable)

--- hobo/lib/hobo/permissions.rb
+++ hobo/lib/hobo/permissions.rb
@@ -206,6 +206,9 @@ module Hobo
       return false unless viewable_by?(user, attribute)
       
       if attribute
+        refl = self.class.reflections[attribute]
+        return false if refl && !refl.options[:accessible]
+        
         attribute = attribute.to_s.sub(/\?$/, '').to_sym
 
         # Try the attribute-specic edit-permission method if there is one
