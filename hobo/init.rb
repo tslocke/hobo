d7ce65de10ef401e32a35a23cbf31c3abc9dcc8c
Tom Locke <tom@tomlocke.com>
2009-03-13 10:46:09 +0000

    Removed monkey path for finding generators plugins that are symlinked. Fixes problem with selenium generator not found

--- hobo/init.rb
+++ hobo/init.rb
@@ -1,9 +1,2 @@
 require File.dirname(__FILE__) + "/lib/hobo"
 require 'rails_generator'
-
-# 'orrible but 'reative 'ack to allow generators to be in symlinked plugins
-Rails::Generator::PathSource.class_eval do
-  def path
-    @path.gsub('**', '*/**')
-  end
-end
