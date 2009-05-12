81dc7c050ab1cce4a2c68ab9136d3900eaab4837
Bryan Larsen <bryan@larsen.st>
2009-05-12 09:33:27 -0400

    Bug 419: fix Enumerable::group_by monkey patch to work with Rails 2.3

--- hobo/lib/hobo.rb
+++ hobo/lib/hobo.rb
@@ -209,11 +209,13 @@ end
 
 module ::Enumerable
   def group_by_with_metadata(&block)
-    group_by_without_metadata(&block).each do |k,v|
+    r=group_by_without_metadata(&block)
+    r.each do |k,v|
       v.origin = origin
       v.origin_attribute = origin_attribute
       v.member_class = member_class
     end
+    r
   end
   alias_method_chain :group_by, :metadata
 end
