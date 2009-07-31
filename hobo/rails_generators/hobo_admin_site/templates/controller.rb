1fb5305d2eee3ec0a86f8bbbe02b6fe5640aeacc
Tom Locke <tom@tomlocke.com>
2009-07-31 11:25:32 +0100

    Admin site generator -- non-admins were not locked out of the admin site

--- hobo/rails_generators/hobo_admin_site/templates/controller.rb
+++ hobo/rails_generators/hobo_admin_site/templates/controller.rb
@@ -2,4 +2,12 @@ class <%= subsite_name %>::<%= subsite_name %>SiteController < ApplicationContro
 
   hobo_controller
   
+  before_filter :admin_required
+  
+  private
+  
+  def admin_required
+    redirect_to home_page unless current_user.administrator?
+  end  
+  
 end
