tslocke/hobo

commit 7fd8f573d06d8822893c491f2450fea5657a94d4

add index on STI type column

hobofields/lib/hobo_fields/model_extensions.rb
@@ -22,6 +22,7 @@ module HoboFields
       fields do |f|
         f.field(inheritance_column, :string)
       end
+      index(inheritance_column)
       super
     end
 

commit 88a6af434cd4ab58c3d42fff099ecb46b4a94b00

7fd8f57 broke the tests.  Fix.

hobofields/lib/hobo_fields/model_extensions.rb
@@ -22,7 +22,7 @@ module HoboFields
       fields do |f|
         f.field(inheritance_column, :string)
       end
-      index(inheritance_column)
+      index(inheritance_column) unless index_specs.*.fields.include?([inheritance_column])
       super
     end
 

hobofields/test/migration_generator.rdoctest
@@ -493,6 +493,7 @@ Set the table name back to what it should be and confirm we're in sync:
 ### Rename a table
 
 As with renaming columns, we have to tell the migration generator about the rename. Here we create a new class 'Advertisement', and tell ActiveRecord to forget about the Advert class. This requires code that shouldn't be shown to impressionable children.
+{.hidden}
 
     >>
      def nuke_model_class(klass)
@@ -553,17 +554,23 @@ Adding a subclass or two should introduce the 'type' column and no other changes
          end
         >> up, down = HoboFields::MigrationGenerator.run
         >> up
-        => "add_column :adverts, :type, :string"
+        =>
+         "add_column :adverts, :type, :string
+         add_index :adverts, [:type]"
         >> down
-        => "remove_column :adverts, :type"
+        => 
+         "remove_column :adverts, :type
+         remove_index :adverts, :name => :index_adverts_on_type"
 
 Cleanup
+{.hidden}
 
         >> Advert.field_specs.delete(:type)
-
         >> nuke_model_class(SuperFancyAdvert)
         >> nuke_model_class(FancyAdvert)
+        >> Advert.index_specs.delete_if {|spec| spec.fields==["type"]}
 {.hidden}
+
 
 ## Coping with multiple changes
 