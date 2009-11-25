f2e8922d014d7c4eb64f136cc9b81a0c1912220d
Matt Jones <al2o3cr@gmail.com>
2009-11-24 21:13:51 -0500

    fix miniscule typo

--- hobofields/rails_generators/hobo_migration/hobo_migration_generator.rb
+++ hobofields/rails_generators/hobo_migration/hobo_migration_generator.rb
@@ -134,7 +134,7 @@ class HoboMigrationGenerator < Rails::Generator::Base
       opt.separator 'Options:'
       
       opt.on("--force-drop",   "Don't prompt with 'drop or rename' - just drop everything") { |v| options[:force_drop] = true }
-      opt.on("--default-name", "Dont' prompt for a migration name - just pick one")         { |v| options[:default_name] = true }
+      opt.on("--default-name", "Don't prompt for a migration name - just pick one")         { |v| options[:default_name] = true }
       opt.on("--generate",     "Don't prompt for action - generate the migration")          { |v| options[:action] = 'g' }
       opt.on("--migrate",      "Don't prompt for action - generate and migrate")            { |v| options[:action] = 'm' }
     end
