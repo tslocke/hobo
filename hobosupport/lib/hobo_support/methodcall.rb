00a6bb64f38ada0bb7085d0e4378e4296520b322
Colin Kelley <ColinKelley@ColinKelley.local>
2009-12-12 11:49:27 -0800

    Simplified _? semantics and implementation. Updated docs and tests and fixed a couple spelling mistakes

--- hobosupport/lib/hobo_support/methodcall.rb
+++ hobosupport/lib/hobo_support/methodcall.rb
@@ -1,6 +1,6 @@
 # The dot operator calls methods on objects. Power Dots are dots with extra features
 #
-#   .? calls a method if the reciever is not nil, returns nil
+#   .? calls a method if the receiver is not nil, returns nil
 #   otherwise. We have to write it ._?. in order to be valid Ruby
 #
 #   .try. calls a mehod only if the recipient resonds to that method
@@ -56,29 +56,7 @@ end
 class SafeNil
   include Singleton
 
-  DONT_REDEFINE_METHODS = "__id__", "__send__", "object_id"
-
-  NIL_RESPONSE_METHODS = ["to_s", "to_json", "to_yaml", "__id__", "__is_a__", "__metaclass__", "__send__"]
-
-  (NIL_RESPONSE_METHODS - DONT_REDEFINE_METHODS).each do |method|
-    # can't use define_method with a block
-    eval "
-      def #{method}(*args, &b)
-        nil.send(:#{method}, *args, &b)
-      end"
-  end
-
-  (instance_methods.map{|m| m.to_s} - NIL_RESPONSE_METHODS - DONT_REDEFINE_METHODS).each do |method|
-    # can't use define_method with a block
-    eval "
-      def #{method}(*args, &b)
-        nil
-      end"
-  end
-
-  def to_s
-    ""
-  end  
+  instance_methods.each { |m| undef_method m unless m[0,2] == '__' }
 
   def method_missing(method, *args, &b)
     return nil
