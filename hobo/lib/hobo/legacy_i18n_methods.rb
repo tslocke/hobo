b15b3fabdf6c114cbdb67ca0887b9e3116dcac9b
Domizio Demichelis <dd.nexus@gmail.com>
2010-05-02 20:31:39 -0400

    improved warning and doc of the legacy methods

--- hobo/lib/hobo/legacy_i18n_methods.rb
+++ hobo/lib/hobo/legacy_i18n_methods.rb
@@ -5,24 +5,34 @@ module Hobo
   module LegacyI18nMethods
   
     def load_legacy_i18n(method, *args)
-      Rails.logger.warn "The ViewHints methods 'model_name', 'model_name_plural' and 'field_name' \
-have been deprecated. Please, set the names in a locale file and use the ActiveRecord::Base methods \
-'human_name' and 'human_attribute_name' to retrieve them."
-      # TODO: add a file with instructions about how to use canonical i18n in hobo
+      Rails.logger.warn "The method '#{method}' is deprecated. "\
+                        "Please, see Hobo::LegacyMethods.#{method} documentation."
       require 'hobo/legacy_i18n'
       send method, *args
     end
     
+    # Deprecated method. Use ActiveRecord::Base.human_name 
+    # and set a the activerecord.models.<model_name> key in a locale file.
     def model_name(*args)
       load_legacy_i18n :model_name, *args
     end
     
+    # Deprecated method. Use ActiveRecord::Base.human_name passing a :count option, 
+    # and set a the activerecord.models.<model_name> key in a locale file.
     def model_name_plural(*args)
       load_legacy_i18n :model_name_plural, *args
     end
     
+    # Deprecated method. Use ActiveRecord::Base.human_attribute_name 
+    # and set a the activerecord.attributes.<model_name>.<field_name> key in a locale file.
     def field_name(*args)
       load_legacy_i18n :field_name, *args
+    end
+    
+    # Deprecated method. You can set the field names by setting
+    # the activerecord.attributes.<model_name>.<field_name> keys in a locale file
+    def field_names(*args)
+      load_legacy_i18n :field_names, *args
     end
     
   end
