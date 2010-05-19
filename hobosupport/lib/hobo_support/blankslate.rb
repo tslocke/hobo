8f6fffb9440f6eea40fc735ff33a41ecf7f4870d
Bryan Larsen <bryan@larsen.st>
2010-05-19 13:19:17 -0400

    [#724 state:resolved] remove private methods from BlankSlate

--- hobosupport/lib/hobo_support/blankslate.rb
+++ hobosupport/lib/hobo_support/blankslate.rb
@@ -1,7 +1,7 @@
 # Define BlankSlate in case ActiveSupport aint present
 unless defined? BlankSlate
   class BlankSlate
-    instance_methods.reject { |m| m =~ /^__/ || m.to_s == 'object_id' }.each { |m| undef_method m }
+    (instance_methods+protected_instance_methods+private_instance_methods).reject { |m| m =~ /^__/ || m.to_s == 'object_id' }.each { |m| undef_method m }
     def initialize(me)
       @me = me
     end
