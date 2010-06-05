6ab832319069f50bbca383ab51dcb424e2da5c7c
Andrew Bloomgarden <stalkingtiger@gmail.com>
2010-06-05 00:50:02 -0700

    More DRYML reversion for rails2and3.

--- dryml/rails/init.rb
+++
@@ -1,6 +0,0 @@
-# If Hobo is defined, assume that it enables DRYML.  Otherwise enable ourselves
-begin
-  Hobo  # can't use const_defined? in case it's autoloaded.
-rescue NameError
-  Dryml.enable
-end
