a25eec5b25144d925dff440778534a0f2821c872
Domizio Demichelis <dd.nexus@gmail.com>
2010-06-14 19:39:08 -0400

    little housekeeping: moved extension code from hobo.rb to hobo_extensions dir

--- hobo/lib/hobo.rb
+++ hobo/lib/hobo.rb
@@ -91,7 +91,10 @@ module Hobo
     public
 
     def enable
-      require 'action_view_extensions/helpers/tag_helper'
+      require 'hobo_extensions/action_view/helpers/tag_helper'
+      require 'hobo_extensions/array'
+      require 'hobo_extensions/enumerable'
+      require 'hobo_extensions/active_record/base'
 
       # Modules that must *not* be auto-reloaded by activesupport
       # (explicitly requiring them means they're never unloaded)
@@ -144,85 +147,6 @@ module Hobo
 end
 
 
-# Add support for type metadata to arrays
-class ::Array
-
-  attr_accessor :member_class, :origin, :origin_attribute
-
-  def to_url_path
-    base_path = origin_object.try.to_url_path
-    "#{base_path}/#{origin_attribute}" unless base_path.blank?
-  end
-
-  def typed_id
-    origin and origin_id = origin.try.typed_id and "#{origin_id}:#{origin_attribute}"
-  end
-
-end
 
 
-module ::Enumerable
-  def group_by_with_metadata(&block)
-    r=group_by_without_metadata(&block)
-    r.each do |k,v|
-      v.origin = origin
-      v.origin_attribute = origin_attribute
-      v.member_class = member_class
-    end
-    r
-  end
-  alias_method_chain :group_by, :metadata
-end
 
-class ActiveRecord::Base
-  class << self
-    # adds a default pluralization for english
-    # useful to avoid to set a locale 'en' file and avoid
-    # to pass around pluralize calls for 'en' defaults in hobo
-    def human_name_with_en_pluralization_default(options={})
-      if I18n.locale.to_s.match(/^en/) 
-        unless options[:count] == 1 || options[:count].blank?
-          default = options[:default].class.eql?(Array) ? options[:default] : [options[:default]]
-          default << self.name.underscore.pluralize.humanize
-          options = options.merge(:default=>default) 
-        end
-      end
-      human_name_without_en_pluralization_default(options)
-    end
-    
-    alias_method_chain :human_name, :en_pluralization_default
-    
-    # adds a default pluralization and singularization for english
-    # useful to avoid to set a locale 'en' file and avoid
-    # to pass around pluralize calls for 'en' defaults in hobo
-    def human_attribute_name_with_en_pluralization_default(attribute, options={})
-      if I18n.locale.to_s.match(/^en/) 
-        default = options[:default].class.eql?(Array) ? options[:default] : [options[:default]]
-        if options[:count] == 1 || options[:count].blank?
-          default << attribute.to_s.singularize.humanize
-        else
-          default << attribute.to_s.pluralize.humanize
-        end
-        options = options.merge(:default=>default) 
-      end
-      human_attribute_name_without_en_pluralization_default(attribute, options)
-    end
-    
-    alias_method_chain :human_attribute_name, :en_pluralization_default
-
-    # Similar to human_name_attributes, this method retrieves the localized help string 
-    # of an attribute if it is defined as the key "activerecord.attribute_help.<attribute_name>", 
-    # otherwise it returns "".
-    def attribute_help(attribute_key_name, options = {})
-      defaults = self_and_descendants_from_active_record.map do |klass|
-        :"#{klass.name.underscore}.#{attribute_key_name}"
-      end
-      defaults << options[:default] if options[:default]
-      defaults.flatten!
-      defaults << ""
-      options[:count] ||= 1
-      I18n.translate(defaults.shift, options.merge(:default => defaults, :scope => [:activerecord, :attribute_help]))
-    end
-
-  end
-end
---
+++ hobo/lib/hobo_extensions/array.rb
@@ -0,0 +1,15 @@
+# Add support for type metadata to arrays
+class Array
+
+  attr_accessor :member_class, :origin, :origin_attribute
+
+  def to_url_path
+    base_path = origin_object.try.to_url_path
+    "#{base_path}/#{origin_attribute}" unless base_path.blank?
+  end
+
+  def typed_id
+    origin and origin_id = origin.try.typed_id and "#{origin_id}:#{origin_attribute}"
+  end
+
+end
