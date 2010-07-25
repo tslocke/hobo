cd9a09148c5860dcf2ff78fe0da56dce5e26052c
Domizio Demichelis <dd.nexus@gmail.com>
2010-07-25 19:39:37 -0400

    added a pristine testapp for easy copying before testing

---
+++ hobofields/test/pristine_testapp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = 'bb06b75a87e000040d1cd030a2676d550e42babeabfd282b8f9295a818886fce829a228f3dfd492046b4805ff6cb29ef215e35a47154875184afdc9d5f2eb11c'
