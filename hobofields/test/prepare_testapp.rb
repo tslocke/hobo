877db24c873f4a8fb2455f787779b5bc4a6ac7d1
Domizio Demichelis <dd.nexus@gmail.com>
2010-07-25 21:40:42 -0400

    better prepare_testapp

--- hobofields/test/prepare_testapp.rb
+++ hobofields/test/prepare_testapp.rb
@@ -1,8 +1,8 @@
 require 'fileutils'
 TEST_APP_ROOT = File.expand_path('../testapp',__FILE__)
+FileUtils.rm_rf TEST_APP_ROOT # remove any zombie app left by an error
 FileUtils.cp_r File.expand_path('../pristine_testapp',__FILE__), TEST_APP_ROOT
 Dir.chdir( TEST_APP_ROOT )
-require 'rails'
 require 'config/environment'
 require 'rails/generators'
 Rails::Generators.configure!
