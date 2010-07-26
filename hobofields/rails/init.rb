5ac2b10f907defcb6c6715b00e7fc56fe83af919
Domizio Demichelis <dd.nexus@gmail.com>
2010-07-25 20:54:57 -0400

    removed the init file

--- hobofields/rails/init.rb
+++
@@ -1,9 +0,0 @@
-module ::HoboFields
-  class << self
-    attr_accessor :rails_initializer
-  end
-end
-::HoboFields.rails_initializer = initializer
-
-require File.dirname(__FILE__) + "/../lib/hobofields"
-HoboFields.enable
