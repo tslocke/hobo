5f8d379a3a74e4ead4aa2a08a509bbfe5b0f804b
Domizio Demichelis <dd.nexus@gmail.com>
2010-07-29 11:38:48 -0400

    removed the commented hobo_fields/railtie initializer
    all the dirs under app/ are automatically added to the eager_load_paths by rails 3, and the eager_load! method automatically does the same that is in the initializer block

--- hobofields/lib/hobo_fields/railtie.rb
+++ hobofields/lib/hobo_fields/railtie.rb
@@ -4,14 +4,6 @@ require 'rails'
 module HoboFields
   class Railtie < Rails::Railtie
 
-#    initializer :eager_load_rich_types, :after => :set_autoload_paths do |app|
-#      app.config.eager_load_paths += %W( #{app.config.root}/app/rich_types )
-#      Dir[app.config.root.join('app', 'rich_types', '*.rb')].each do |f|
-#        # TODO: should we complain if field_types doesn't get a new value? Might be useful to warn people if they're missing a register_type
-#        require_dependency f
-#      end
-#    end
-
     ActiveSupport.on_load(:active_record) do
       # Add the fields declaration to ActiveRecord::Base
       include HoboFields::FieldsDeclaration
