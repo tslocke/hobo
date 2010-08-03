4d7f7071789d08dbef27686d8d91d1283ad8ce0e
Domizio Demichelis <dd.nexus@gmail.com>
2010-08-03 13:57:27 -0400

    refactory of Hobo::Routes

--- hobo/lib/hobo/routes.rb
+++ hobo/lib/hobo/routes.rb
@@ -8,19 +8,22 @@ module Hobo
 
       def linkable_key(klass, action, options)
         subsite = options[:subsite] || options['subsite']
-        method  = options[:method]  || options['method']
-        opts = options.map { |k, v| "#{k}=#{v}" unless v.blank? }.compact.join(',')
-        "#{subsite}/#{klass.name}/#{action}/#{method}"
+        method  = options[:method]  || options['method'] || :get
+        [ subsite, klass.name, action, method ]
       end
 
       def linkable!(klass, action, options={})
-        options[:method] ||= :get
         @linkable << linkable_key(klass, action, options)
       end
 
       def linkable?(klass, action, options={})
-        options[:method] ||= :get
         @linkable.member? linkable_key(klass, action, options)
+      end
+
+      def models_with(action)
+        @linkable.map do |k|
+          (k[2] == action) ? k[1].constantize : nil
+        end.compact
       end
 
     end
