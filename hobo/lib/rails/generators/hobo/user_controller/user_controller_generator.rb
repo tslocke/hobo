10ac926d24ea9ce5c249eaa5c8e8884b8b591fcf
Domizio Demichelis <dd.nexus@gmail.com>
2010-08-05 20:29:08 -0400

    added hobo:user_controller

---
+++ hobo/lib/rails/generators/hobo/user_controller/user_controller_generator.rb
@@ -0,0 +1,29 @@
+module Hobo
+  class UserControllerGenerator < Rails::Generators::NamedBase
+    source_root File.expand_path('../templates', __FILE__)
+    include Hobo::ControllerGeneratorModule
+
+    def self.banner
+      "rails generate hobo:user_controller #{self.arguments.map(&:usage).join(' ')} [--invite-only]"
+    end
+
+    class_option :invite_only, :type => :boolean
+
+    def generate_controller
+      template 'controller.rb.erb', File.join('app/controllers',"#{file_path}_controller.rb")
+    end
+
+    def generate_accept_invitation
+      if invite_only?
+        template "accept_invitation.dryml", File.join('app/views', file_path, "accept_invitation.dryml")
+      end
+    end
+
+  private
+
+    def invite_only?
+      options[:invite_only]
+    end
+
+  end
+end
