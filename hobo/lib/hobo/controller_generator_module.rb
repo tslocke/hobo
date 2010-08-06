49dc7c4b82d7cc4d69da67f93358facabe8fe4af
Domizio Demichelis <dd.nexus@gmail.com>
2010-08-05 20:28:41 -0400

    added hobo:controller generator

---
+++ hobo/lib/hobo/controller_generator_module.rb
@@ -0,0 +1,26 @@
+module Hobo
+  ControllerGeneratorModule = classy_module do
+
+    check_class_collision :suffix => 'Controller'
+
+    def self.banner
+      "rails generate hobo:cotroller #{self.arguments.map(&:usage).join(' ')}"
+    end
+
+    def generate_controller
+      if class_path.length == 1 and
+        subsite = class_path.first and
+        File.exist?(File.join('app/controllers', class_path, "#{subsite}_site_controller.rb"))
+        @subsite = subsite.camelize
+      end
+      template 'controller.rb.erb', File.join('app/controllers',"#{file_path}_controller.rb")
+    end
+
+    def generate_helper
+      invoke 'helper', [name], options
+    end
+
+    hook_for :test_framework, :as => :controller
+
+  end
+end
--- hobo/rails_generators/hobo_model_controller/templates/controller.rb
+++
@@ -1,7 +0,0 @@
-class <%= class_name %>Controller < <%= subsite ? "#{subsite}::#{subsite}SiteController" : "ApplicationController" %>
-
-  hobo_model_controller
-
-  auto_actions :all
-
-end
