c9249f13343463932270949fb318954fc8f8ecd5
Domizio Demichelis <dd.nexus@gmail.com>
2010-08-07 09:49:42 -0400

    added default name 'admin' to the hobo:admin_subsite generator

--- hobo/lib/rails/generators/hobo/admin_subsite/admin_subsite_generator.rb
+++ hobo/lib/rails/generators/hobo/admin_subsite/admin_subsite_generator.rb
@@ -5,18 +5,21 @@ module Hobo
     include Hobo::Generators::SubsiteModule
     include Hobo::Generators::InviteOnlyModule
 
+    # overrides the default
+    argument :name, :type => :string, :default => 'admin', :optional => true
+
     def self.banner
       "rails generate hobo:admin_subsite #{self.arguments.map(&:usage).join(' ')} [options]"
     end
 
     def generate_admin_css
-      template "admin.css", File.join('public/stylesheets/admin.css')
+      template "admin.css", File.join("public/stylesheets/#{file_name}.css")
     end
 
     def generate_for_invite_only
       return unless invite_only?
-      invoke "hobo:controller", ["admin/user"]
-      template "users_index.dryml", "app/views/admin/users/index.dryml"
+      invoke "hobo:controller", ["#{file_name}/user"]
+      template "users_index.dryml", "app/views/#{file_name}/users/index.dryml"
     end
 
   end
