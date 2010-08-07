75cdaa548e0edcb1497b11165143617ee122267e
Domizio Demichelis <dd.nexus@gmail.com>
2010-08-07 11:58:56 -0400

    added hobo:user_resource generator

---
+++ hobo/lib/rails/generators/hobo/user_resource/user_resource_generator.rb
@@ -0,0 +1,20 @@
+module Hobo
+  class UserResourceGenerator < Rails::Generators::NamedBase
+
+    # overrides the default
+    argument :name, :type => :string, :default => 'user', :optional => true
+
+    def self.banner
+      "rails generate hobo:user_resource [NAME=user] [options]"
+    end
+
+    def generate_hobo_user_controller
+      invoke 'hobo:user_controller', [name.pluralize], options
+    end
+
+    def generate_hobo_model
+      invoke 'hobo:user_model', [name.singularize], options
+    end
+
+  end
+end
