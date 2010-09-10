370a5ff9fed097ab0c04b667fb02ddefb8cd9b6f
Domizio Demichelis <dd.nexus@gmail.com>
2010-09-10 14:45:31 -0400

    fix for linkable? not working properly

--- hobo/lib/hobo/routes.rb
+++ hobo/lib/hobo/routes.rb
@@ -3,26 +3,27 @@ module Hobo
     class << self
 
       def reset_linkables
-        @linkable = Set.new
+        @linkable_keys = Set.new
       end
 
       def linkable_key(klass, action, options)
         subsite = options[:subsite] || options['subsite']
         method  = options[:method]  || options['method'] || :get
-        [ subsite, klass.name, action, method ]
+        [ subsite, klass.name, action, method ].join('/')
       end
 
       def linkable!(klass, action, options={})
-        @linkable << linkable_key(klass, action, options)
+        @linkable_keys << linkable_key(klass, action, options)
       end
 
       def linkable?(klass, action, options={})
-        @linkable.member? linkable_key(klass, action, options)
+        @linkable_keys.member? linkable_key(klass, action, options)
       end
 
-      def models_with(action)
-        @linkable.map do |k|
-          (k[2] == action) ? k[1].constantize : nil
+      def models_with(wanted_action)
+        @linkable_keys.map do |k|
+          subsite, class_name, action, method = k.split('/')
+          (action == wanted_action) ? class_name.constantize : nil
         end.compact
       end
 
