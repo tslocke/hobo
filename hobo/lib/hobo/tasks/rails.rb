c4580154023d6dad0c401f18f0cb7819e232976b
Domizio Demichelis <dd.nexus@gmail.com>
2010-09-16 13:10:05 -0400

    removed obsolete files

--- hobo/lib/hobo/tasks/rails.rb
+++
@@ -1,4 +0,0 @@
-# This file is used to load the Hobo rake tasks when running Hobo as a gem
-
-tasks_home = File.join File.dirname(__FILE__), "..", "..", "..", "tasks"
-Dir["#{tasks_home}/**/*.rake"].sort.each { |ext| load ext }
