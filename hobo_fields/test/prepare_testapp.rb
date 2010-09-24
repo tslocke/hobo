e3d0f1ef2aa1288ea8269e6e127b6e6e76525ae4
Domizio Demichelis <dd.nexus@gmail.com>
2010-09-24 14:04:57 -0400

    changed prepare_testapp.rb helper

--- hobo_fields/test/prepare_testapp.rb
+++ hobo_fields/test/prepare_testapp.rb
@@ -1,8 +1,6 @@
 require 'fileutils'
-TEST_APP_ROOT = File.expand_path('../testapp',__FILE__)
-FileUtils.rm_rf TEST_APP_ROOT # remove any zombie app left by an error
-FileUtils.cp_r File.expand_path('../pristine_testapp',__FILE__), TEST_APP_ROOT
-Dir.chdir( TEST_APP_ROOT )
+system %(rake prepare_testapp)
+FileUtils.chdir '/tmp/hobo_fields_testapp'
 require 'config/environment'
 require 'rails/generators'
 Rails::Generators.configure!
