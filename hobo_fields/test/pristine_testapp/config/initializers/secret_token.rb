b09821b9b0d984ba3843a044bc04678e78c80fbf
Domizio Demichelis <dd.nexus@gmail.com>
2010-09-24 11:48:43 -0400

    removed pristine_testapp

--- hobo_fields/test/pristine_testapp/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Testapp::Application.config.secret_token = '8fb39ea0d66f13179233d77c3f2cb3acec3d154aa38494ffbabcf380aa2a25ff8bf63c1a41a78295465db589dcb480d397b77a57b3dcb8558cea27e399ce9efc'
