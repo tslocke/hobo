5bf6b6402c045aad4fc86bfb5c6f9ea19160fe46
Domizio Demichelis <dd.nexus@gmail.com>
2010-10-04 18:55:51 -0400

    xss: improved the safe_join logic [#822 state:resolved]

--- hobo_support/lib/hobo_support/xss.rb
+++ hobo_support/lib/hobo_support/xss.rb
@@ -1,12 +1,13 @@
+require 'active_support/core_ext/string/output_safety'
 class Array
   def safe_join(sep=$,)
-    if (sep.nil? || sep.html_safe?) && self.all? {|i| i.html_safe?}
-      self.join(sep).html_safe
+    if self.all? {|i| i.html_safe?}
+      self.join(ERB::Util.html_escape(sep)).html_safe
     else
       self.join(sep)
     end
   end
 end
-      
-    
-    
+
+
+
