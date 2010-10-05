40b3b78251b66ee6ad4dabd3f7b10e1a3a166b50
Bryan Larsen <bryan@larsen.st>
2010-10-05 00:00:21 -0400

    hmmm, those requires really should be in dryml...

--- lib/doc.rb
+++ lib/doc.rb
@@ -1,6 +1,8 @@
 require 'rubygems'
 require 'maruku'
 require 'dryml'
+require 'action_view'
+require 'action_controller'
 
 taglibs = Dir["../taglibs/**/*.dryml"].reject {|filename|
   File.basename(filename).match(/^hobo-jquery-.*/)
