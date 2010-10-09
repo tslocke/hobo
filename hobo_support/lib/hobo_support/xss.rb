590e005a4cbb91edc23c91e67714cc37b947649e
Domizio Demichelis <dd.nexus@gmail.com>
2010-10-09 07:22:25 -0400

    xss: better safe_join

--- hobo_support/lib/hobo_support/xss.rb
+++ hobo_support/lib/hobo_support/xss.rb
@@ -1,11 +1,9 @@
 require 'active_support/core_ext/string/output_safety'
 class Array
+  # it always returns an html_safe? string preserving the html_safe?
+  # items and separator, excaping the rest
   def safe_join(sep=$,)
-    if self.all? {|i| i.html_safe?}
-      self.join(ERB::Util.html_escape(sep)).html_safe
-    else
-      self.join(sep)
-    end
+    map {|i| ERB::Util.html_escape(i)}.join(ERB::Util.html_escape(sep)).html_safe
   end
 end
 
