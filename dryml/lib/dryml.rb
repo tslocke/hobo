tslocke/hobo

commit 3ae774c5304a3a1f822c8452e71bfd45af6b6075

fixed ajax_update_response for pages and removed the use of @template and _evaluate_assigns_and_ivars [#829 state:resolved]

dryml/lib/dryml.rb
@@ -90,7 +90,16 @@ module Dryml
       prepare_view!(view)
       included_taglibs = ([APPLICATION_TAGLIB] + application_taglibs() + [subsite_taglib(page)] + controller_taglibs(view.controller.class)).compact
 
-      if page.ends_with?(EMPTY_PAGE)
+      # filename is blank when called from controller's ajax_update_response
+      if filename.blank?
+        opt = ActionController::Routing::Routes.recognize_path(page)
+        filename = view.view_paths.find( opt[:action],
+                                         opt[:controller],
+                                         false,
+                                         view.lookup_context.instance_variable_get('@details')).identifier
+      end
+
+      if identifier.starts_with?('dryml-page-tag:') || page_path.ends_with?(EMPTY_PAGE)
         controller_class = view.controller.class
         @tag_page_renderer_classes[controller_class.name] ||=
           make_renderer_class("", page, local_names, DEFAULT_IMPORTS, included_taglibs)
@@ -168,7 +177,7 @@ module Dryml
       # Not sure why this isn't done for me...
       # There's probably a button to press round here somewhere
       for var in %w(@flash @cookies @action_name @_session @_request @request_origin
-                    @template @request @ignore_missing_templates @_headers @variables_added
+                    @request @ignore_missing_templates @_headers @variables_added
                     @_flash @response @template_class
                     @_cookies @before_filter_chain_aborted @url
                     @_response @template_root @headers @_params @params @session)

dryml/lib/dryml/railtie/page_tag_resolver.rb
@@ -11,7 +11,7 @@ module Dryml
         tag_name = @controller.dryml_fallback_tag || name.dasherize + '-page'
         text = @controller.call_dryml_tag(tag_name)
         return [] unless text
-        [ActionView::Template.new(text, "dryml-tag:#{tag_name}", Dryml::Railtie::PageTagHandler, details)]
+        [ActionView::Template.new(text, "dryml-page-tag:#{tag_name}", Dryml::Railtie::PageTagHandler, details)]
       end
 
     end

hobo/lib/hobo/controller.rb
@@ -70,8 +70,7 @@ module Hobo
 
 
     def ajax_update_response(page_path, render_specs, results={})
-      @template.send(:_evaluate_assigns_and_ivars)
-      renderer = Dryml.page_renderer(@template, [], page_path) if page_path
+      renderer = Dryml.page_renderer(view_context, [], page_path) if page_path
 
       render :update do |page|
         page << "var _update = typeof Hobo == 'undefined' ? Element.update : Hobo.updateElement;"
@@ -91,7 +90,7 @@ module Hobo
         end
         page << renderer.part_contexts_storage if renderer
       end
-    end
+   end
 
 
     def dryml_context
@@ -113,10 +112,7 @@ module Hobo
 
 
     def tag_renderer
-      @tag_renderer ||= begin
-        @template.send(:_evaluate_assigns_and_ivars)
-        Dryml.empty_page_renderer(@template)
-      end
+      @tag_renderer ||= Dryml.empty_page_renderer(view_context)
     end
 
 

commit 600ffc4e4c506c6c7f6c0712c3c4999c7d932a36

fixes a bug introduced in 3ae774c5304a3 [#829]

dryml/lib/dryml.rb
@@ -90,16 +90,14 @@ module Dryml
       prepare_view!(view)
       included_taglibs = ([APPLICATION_TAGLIB] + application_taglibs() + [subsite_taglib(page_path)] + controller_taglibs(view.controller.class)).compact
 
-      # filename is blank when called from controller's ajax_update_response
-      if identifier.blank?
+      if identifier.blank? && ! page_path.ends_with?(EMPTY_PAGE)
         opt = ActionController::Routing::Routes.recognize_path(page_path)
         identifier = view.view_paths.find( opt[:action],
                                            opt[:controller],
                                            false,
                                            view.lookup_context.instance_variable_get('@details')).identifier
       end
-
-      if identifier.starts_with?('dryml-page-tag:') || page_path.ends_with?(EMPTY_PAGE)
+      if page_path.ends_with?(EMPTY_PAGE) || identifier.starts_with?('dryml-page-tag:')
         controller_class = view.controller.class
         @tag_page_renderer_classes[controller_class.name] ||=
           make_renderer_class("", page_path, local_names, DEFAULT_IMPORTS, included_taglibs)