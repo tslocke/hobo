87da11b1cc3b3d8a2efa7853df4bd193db4cf749
Domizio Demichelis <dd.nexus@gmail.com>
2010-11-07 10:16:33 -0400

    fix for object_url with nil origin (generated by the :origin Relation accessor)

--- hobo/lib/hobo/helper.rb
+++ hobo/lib/hobo/helper.rb
@@ -58,7 +58,7 @@ module Hobo
       options[:subsite] ||= self.subsite
       subsite, method = options.get :subsite, :method
 
-      if obj.respond_to?(:member_class) && obj.respond_to?(:origin)
+      if obj.respond_to?(:member_class) && obj.respond_to?(:origin) && obj.origin
         # Asking for URL of a collection, e.g. category/1/adverts or category/1/adverts/new
 
         refl = obj.origin.class.reverse_reflection(obj.origin_attribute)
