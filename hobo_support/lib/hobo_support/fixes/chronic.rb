5a2fb558180c9a9d678e92388337170a0b876842
Erik Ostrom <erik@echographia.com>
2011-01-29 00:06:19 -0600

    Fix for Chronic.parse options not allowed.
    Chronic.parse takes two parameters, but the Hobo-fixed version took
    only one. Now it passes any parameters beyond the first along to
    Chronic.

--- hobo_support/lib/hobo_support/fixes/chronic.rb
+++ hobo_support/lib/hobo_support/fixes/chronic.rb
@@ -5,7 +5,7 @@ begin
   module Chronic
 
     class << self
-      def parse_with_hobo_fix(s)
+      def parse_with_hobo_fix(s, *options)
         if s =~ /^\s*\d+\s*(st|nd|rd|th)\s+[a-zA-Z]+(\s+\d+)?\s*$/
           s = s.sub(/\s*\d+(st|nd|rd|th)/) {|s| s[0..-3]}
         end
@@ -13,7 +13,7 @@ begin
         # Chronic can't parse '1/1/2008 1:00' or '1/1/2008 1:00 PM',
         # so convert them to '1/1/2008 @ 1:00' and '1/1/2008 @ 1:00 PM'
         s = s.sub(/^\s*(\d+\/\d+\/\d+)\s+(\d+:\d+.*)/, '\1 @ \2')
-        parse_without_hobo_fix(s)
+        parse_without_hobo_fix(s, *options)
       end
       alias_method_chain :parse, :hobo_fix
     end
