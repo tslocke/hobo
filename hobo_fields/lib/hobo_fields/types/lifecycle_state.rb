7f3cd00707efda648a875b36f4a17a6aa2064618
Domizio Demichelis <dd.nexus@gmail.com>
2011-02-17 16:43:16 -0400

    fix for model_name call on wrong receiver

--- hobo_fields/lib/hobo_fields/types/lifecycle_state.rb
+++ hobo_fields/lib/hobo_fields/types/lifecycle_state.rb
@@ -9,7 +9,7 @@ module HoboFields
       end
 
       def to_html(xmldoctype = true)
-        I18n.t("activerecord.attributes.#{self.model_name.underscore}.lifecycle.states.#{self}", :default => self).html_safe
+        I18n.t("activerecord.attributes.#{self.class.model_name.underscore}.lifecycle.states.#{self}", :default => self).html_safe
       end
     end
   end
