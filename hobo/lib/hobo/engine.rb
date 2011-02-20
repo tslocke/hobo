tslocke/hobo

commit 3c6267abbb96eb8f7a962bf07a62f64aa8f2e788

fix to use helper.t in console

hobo/lib/hobo/engine.rb
@@ -37,7 +37,7 @@ module Hobo
 
     ActiveSupport.on_load(:action_view) do
       require 'hobo/extensions/action_view/tag_helper'
-      require 'hobo/extensions/action_view/translation_helper'
+      include Hobo::Extensions::ActionView::TranslationHelper
     end
 
     ActiveSupport.on_load(:before_initialize) do

hobo/lib/hobo/extensions/action_view/translation_helper.rb
@@ -1,4 +1,4 @@
-ActionView::Helpers::TranslationHelper.module_eval do
+module Hobo::Extensions::ActionView::TranslationHelper
 
   include Hobo::Helper::Translations::Normalizer
 

commit aae682e5c71759757067efb1d5e18d836f657db7

fix translate helper bug introduced in commit 3c6267abbb96eb8f7a962bf07a62f64aa8f2e788

hobo/lib/hobo/engine.rb
@@ -37,7 +37,8 @@ module Hobo
 
     ActiveSupport.on_load(:action_view) do
       require 'hobo/extensions/action_view/tag_helper'
-      include Hobo::Extensions::ActionView::TranslationHelper
+      require 'hobo/extensions/action_view/translation_helper'
+      include Hobo::Helper::Translations::Normalizer
     end
 
     ActiveSupport.on_load(:before_initialize) do

hobo/lib/hobo/extensions/action_view/translation_helper.rb
@@ -1,6 +1,4 @@
-module Hobo::Extensions::ActionView::TranslationHelper
-
-  include Hobo::Helper::Translations::Normalizer
+ActionView::Helpers::TranslationHelper.module_eval do
 
   # simple wrapper around the translate helper
   # it implements a dryml <translate> and a <t> tag