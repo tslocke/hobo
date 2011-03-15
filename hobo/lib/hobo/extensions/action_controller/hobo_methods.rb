4ed7f35a80148148c7d99f0ea750f3774e78ac1c
Paul <tacid@tacid.kiev.ua>
2011-03-15 17:22:44 +0200

    Revert "Returned hobo_model_controller parameter model"
    This reverts commit 9d4085aa0cf103c2fa1b0e8450a7af439238293f.

--- hobo/lib/hobo/extensions/action_controller/hobo_methods.rb
+++ hobo/lib/hobo/extensions/action_controller/hobo_methods.rb
@@ -5,8 +5,7 @@ ActionController::Base.class_eval do
     include Hobo::Controller::User
   end
 
-  def self.hobo_model_controller(model=nil)
-    @model = model
+  def self.hobo_model_controller
     include Hobo::Controller::Model
   end
 
