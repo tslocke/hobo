749b7b480ac8ffac074eef3f2b2e0cb36b4430e0
Domizio Demichelis <dd.nexus@gmail.com>
2011-04-08 08:56:19 -0400

    moved #access_denied to ActionController::Base for easy overriding [#922 state:resolved]

--- hobo/lib/hobo/extensions/action_controller/hobo_methods.rb
+++ hobo/lib/hobo/extensions/action_controller/hobo_methods.rb
@@ -17,4 +17,28 @@ ActionController::Base.class_eval do
     base_url
   end
 
+  # moved here from authentication_support.rb for easy overriding
+  # Redirect as appropriate when an access request fails.
+  #
+  # The default action is to redirect to the login screen.
+  #
+  # Override this method in your controllers if you want to have special
+  # behavior in case the user is not authorized
+  # to access the requested action.  For example, a popup window might
+  # simply close itself.
+  def access_denied(user_model)
+    respond_to do |accepts|
+      accepts.html do
+        store_location
+        redirect_to(login_url(user_model))
+      end
+      accepts.xml do
+        headers["Status"]           = "Unauthorized"
+        headers["WWW-Authenticate"] = %(Basic realm="Web Password")
+        render :text => t("hobo.messages.unauthenticated", :default=>["Couldn't authenticate you"]), :status => '401 Unauthorized'
+      end
+    end
+    false
+  end
+
 end
