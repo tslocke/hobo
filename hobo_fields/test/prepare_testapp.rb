4e7b9e603973722f3b11454e0777a339d79e2ca1
Tom Locke <tom@tomlocke.com>
2011-11-09 09:50:47 +0000

    Hobofields - Fixed failing doctests

--- hobo_fields/test/prepare_testapp.rb
+++ hobo_fields/test/prepare_testapp.rb
@@ -1,8 +1,9 @@
 require 'fileutils'
-system %(rake test:prepare_testapp)
 TESTAPP_PATH = '/tmp/hobo_fields_testapp'
+system %(rake test:prepare_testapp TESTAPP_PATH=#{TESTAPP_PATH})
+system %(echo "gem 'BlueCloth'" >> #{TESTAPP_PATH}/Gemfile)
+system %(echo "gem 'RedCloth'" >> #{TESTAPP_PATH}/Gemfile)
 FileUtils.chdir TESTAPP_PATH
 require "#{TESTAPP_PATH}/config/environment"
 require 'rails/generators'
 Rails::Generators.configure!
-
