12b7efe0b12138c6c4e1f0022a16c3c09ed6a30d
Tom Locke <tom@tomlocke.com>
2011-11-11 12:16:10 +0000

    Tweaks to setup of tests to get things working

--- hobo/test/doctest/prepare_testapp.rb
+++ hobo/test/doctest/prepare_testapp.rb
@@ -1,6 +1,6 @@
 require 'fileutils'
-system %(rake test:prepare_testapp)
 TESTAPP_PATH = '/tmp/hobo_testapp'
+system %(rake test:prepare_testapp TESTAPP_PATH=#{TESTAPP_PATH})
 FileUtils.chdir TESTAPP_PATH
 require 'config/environment'
 require 'rails/generators'
