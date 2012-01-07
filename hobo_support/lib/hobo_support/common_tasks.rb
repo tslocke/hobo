34476677f1ed562eb3381b2993b2dae362fb459a
Domizio Demichelis <dd.nexus@gmail.com>
2012-01-07 17:14:43 -0400

    irt rake common task
    - fixed a few problems
    - added rvm automatic support

--- hobo_support/lib/hobo_support/common_tasks.rb
+++ hobo_support/lib/hobo_support/common_tasks.rb
@@ -14,9 +14,20 @@ namespace :test do
   task :prepare_testapp, :force do |t, args|
     if args.force || !File.directory?(TESTAPP_PATH)
       remove_entry_secure( TESTAPP_PATH, true )
-      sh %(#{BIN} new #{TESTAPP_PATH} --skip-wizard)
+      sh %(#{BIN} new #{TESTAPP_PATH} --skip-wizard --skip-bundle)
       chdir TESTAPP_PATH
-      sh %(echo "gem 'irt', :group => :console" >> Gemfile) # to make the bundler happy
+      if ENV['HOBODEV']
+        rvmrc_path = File.join(ENV['HOBODEV'], '.rvmrc')
+        if File.exist?(rvmrc_path)
+          puts %(Copying .rvmrc file)
+          copy_file rvmrc_path, './.rvmrc'
+          sh %(rvm reload) do |ok|
+            puts 'rvm command skipped' unless ok
+          end
+        end
+      end
+      sh %(bundle install --local)
+      sh %(echo "gem 'irt', :group => :development" >> Gemfile) # to make the bundler happy
       sh %(echo "" > app/models/.gitignore) # because git reset --hard would rm the dir
       rm %(.gitignore) # we need to reset everything in a testapp
       sh %(git init && git add . && git commit -m "initial commit")
