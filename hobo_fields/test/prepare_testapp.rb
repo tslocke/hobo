e7b3e0dd5314eb66a4aa7ee719f3a00f0b1c2beb
Domizio Demichelis <dd.nexus@gmail.com>
2012-01-08 10:48:49 -0400

    normalized the prepare_testapp files

--- hobo_fields/test/prepare_testapp.rb
+++ hobo_fields/test/prepare_testapp.rb
@@ -1,5 +1,7 @@
 require 'fileutils'
-TESTAPP_PATH = '/tmp/hobo_fields_testapp'
+require 'tmpdir'
+
+TESTAPP_PATH = ENV['TESTAPP_PATH'] || File.join(Dir.tmpdir, 'hobo_fields_testapp')
 system %(rake test:prepare_testapp TESTAPP_PATH=#{TESTAPP_PATH})
 system %(echo "gem 'bluecloth'" >> #{TESTAPP_PATH}/Gemfile)
 system %(echo "gem 'RedCloth'" >> #{TESTAPP_PATH}/Gemfile)
