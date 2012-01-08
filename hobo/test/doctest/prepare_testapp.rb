3a92e6bda28c0e27daeb3febc4cf41382c5afdd8
Domizio Demichelis <dd.nexus@gmail.com>
2012-01-08 10:58:02 -0400

    fixes for hobo_helper.rdoctest

--- hobo/test/doctest/prepare_testapp.rb
+++ hobo/test/doctest/prepare_testapp.rb
@@ -3,6 +3,7 @@ require 'tmpdir'
 
 TESTAPP_PATH = ENV['TESTAPP_PATH'] || File.join(Dir.tmpdir, 'hobo_testapp')
 system %(rake test:prepare_testapp TESTAPP_PATH=#{TESTAPP_PATH})
+system %(echo "gem 'mocha'" >> #{TESTAPP_PATH}/Gemfile)
 FileUtils.chdir TESTAPP_PATH
 require "#{TESTAPP_PATH}/config/environment"
 require 'rails/generators'
