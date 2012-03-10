497fe7b17e1b0ca21ede5afed6571d0abfed4715
Bryan Larsen <bryan@larsen.st>
2012-03-10 13:23:53 -0500

    rails-dev-tweaks should go into the development group

--- hobo/lib/generators/hobo/dev_tweaks/dev_tweaks_generator.rb
+++ hobo/lib/generators/hobo/dev_tweaks/dev_tweaks_generator.rb
@@ -7,7 +7,7 @@ module Hobo
     desc "install the rails-dev-tweaks plugin & configure it"
     def add_dev_tweaks
       say "Adding rails-dev-tweaks gem"
-      gem_with_comments('rails-dev-tweaks', :version => "~> 0.6.1", :comments => "\n# The asset pipeline in Rails is really slow in development mode.\n# Hobo has a lot of assets, so speed it up with rails-dev-tweaks")
+      gem_with_comments('rails-dev-tweaks', :version => "~> 0.6.1", :comments => "\n# The asset pipeline in Rails is really slow in development mode.\n# Hobo has a lot of assets, so speed it up with rails-dev-tweaks", :group => ":development")
       Bundler.with_clean_env do
         run "bundle install"
       end
