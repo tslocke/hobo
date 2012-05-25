tslocke/hobo

commit 35a991802360136e8c3d4a3dd667ea1766cef7a2

fix for recognize_page_path when using RAILS_RELATIVE_URL_ROOT

hobo/app/helpers/hobo_route_helper.rb
@@ -113,7 +113,10 @@ O            action = :"index_for_#{owner_name}"
       # round tripping params through the router will remove
       # unnecessary params
       url = params[:page_path] || url_for(params)
-      url.gsub!(/^#{ENV['RAILS_RELATIVE_URL_ROOT']}/, "") if ENV['RAILS_RELATIVE_URL_ROOT']
+      if ENV['RAILS_RELATIVE_URL_ROOT']
+        url.gsub!(/^#{ENV['RAILS_RELATIVE_URL_ROOT']}/, "")
+        url.gsub!(/^https?:\/\/.*?#{ENV['RAILS_RELATIVE_URL_ROOT']}/, "")
+      end
       Rails.application.routes.recognize_path(url)
     end
 

commit 12a6731e1d9b67d5f3e7dc534d1ecdb43e65b8a4

35a991802360136e broke some non-relative URL cases.   fix.   Thanks to Henry Barager for helping to track this down.

hobo/app/helpers/hobo_route_helper.rb
@@ -2,7 +2,7 @@ module HoboRouteHelper
   extend HoboHelperBase
   protected
     def base_url
-      ENV['RAILS_RELATIVE_URL_ROOT'] || '/'
+      ENV['RAILS_RELATIVE_URL_ROOT'] || ''
     end
 
     def controller_for(obj)