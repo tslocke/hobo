dc8f3eb092497a01e34a87956ed21f76cd2325fa
Bryan Larsen <bryan@larsen.st>
2012-05-29 16:38:08 -0400

    Some changes to HoboFields to get some of the tests working on Rails 3.2.   Note that you still have to manually tweak the testapp to set config.activerecord.whitelist_attributes = false.

--- hobo_fields/lib/hobo_fields/extensions/active_record/attribute_methods.rb
+++ hobo_fields/lib/hobo_fields/extensions/active_record/attribute_methods.rb
@@ -10,9 +10,31 @@ ActiveRecord::Base.class_eval do
       end
     end
 
+    # Rails 3.2+
+    def internal_attribute_access_code(attr_name, cast_code)
+      access_code = "(v=@attributes[attr_name]) && #{cast_code}"
+
+      unless attr_name == primary_key
+        access_code.insert(0, "missing_attribute(attr_name, caller) unless @attributes.has_key?(attr_name); ")
+      end
+
+      # This is the Hobo hook - add a type wrapper around the field
+      # value if we have a special type defined
+      if can_wrap_with_hobo_type?(attr_name)
+        access_code = "val = begin; #{access_code}; end; wrapper_type = self.class.attr_type(:#{attr_name}); " +
+            "if HoboFields.can_wrap?(wrapper_type, val); wrapper_type.new(val); else; val; end"
+      end
+
+      if cache_attribute?(attr_name)
+        access_code = "@attributes_cache['#{attr_name}'] ||= begin; #{access_code}; end;"
+      end
+
+      "attr_name = '#{attr_name}'; #{access_code}"
+    end
+
+    # Rails 3.1 or earlier
     # Define an attribute reader method.  Cope with nil column.
     def define_read_method(symbol, attr_name, column)
-
       cast_code = column.type_cast_code('v') if column
       access_code = cast_code ? "(v=@attributes['#{attr_name}']) && #{cast_code}" : "@attributes['#{attr_name}']"
 
--- hobo_support/lib/hobo_support/common_tasks.rb
+++ hobo_support/lib/hobo_support/common_tasks.rb
@@ -22,7 +22,9 @@ namespace :test do
         end
       end
       sh %(bundle install)
+      sh %(echo "" >> Gemfile)
       sh %(echo "gem 'irt', :group => :development" >> Gemfile) # to make the bundler happy
+      sh %(echo "gem 'therubyracer'" >> Gemfile)
       sh %(echo "" > app/models/.gitignore) # because git reset --hard would rm the dir
       rm %(.gitignore) # we need to reset everything in a testapp
       sh %(git init && git add . && git commit -m "initial commit")
