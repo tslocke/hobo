127ee34a6712552b190cf2220434e551431a9abb
Bryan Larsen <bryan@larsen.st>
2012-08-01 13:38:13 -0400

    fixup tests to accomodate changes in mocha

--- hobo/test/doctest/prepare_testapp.rb
+++ hobo/test/doctest/prepare_testapp.rb
@@ -3,7 +3,8 @@ require 'tmpdir'
 
 TESTAPP_PATH = ENV['TESTAPP_PATH'] || File.join(Dir.tmpdir, 'hobo_testapp')
 system %(rake test:prepare_testapp TESTAPP_PATH=#{TESTAPP_PATH})
-system %(echo "gem 'mocha'" >> #{TESTAPP_PATH}/Gemfile)
+system %(echo "gem 'mocha', :require => false" >> #{TESTAPP_PATH}/Gemfile)
+system %(echo "gem 'test-unit'" >> #{TESTAPP_PATH}/Gemfile)
 FileUtils.chdir TESTAPP_PATH
 require "#{TESTAPP_PATH}/config/environment"
 require 'rails/generators'
