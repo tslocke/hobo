250f4c85b4c880a6341e3fda326eda46e3d8978e
Bryan Larsen <bryan@larsen.st>
2012-09-06 16:13:19 -0400

    fixup cookbook errors

--- hobo_clean_sidemenu/lib/hobo_clean_sidemenu.rb
+++ hobo_clean_sidemenu/lib/hobo_clean_sidemenu.rb
@@ -4,6 +4,8 @@ module HoboCleanSidemenu
   @@root = Pathname.new File.expand_path('../..', __FILE__)
   def self.root; @@root; end
 
+  EDIT_LINK_BASE = "https://github.com/tablatom/hobo/edit/master/hobo_clean_sidemenu"
+
   require 'hobo_clean_sidemenu/railtie' if defined?(Rails)
 
   class Engine < ::Rails::Engine
