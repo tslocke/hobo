03b7167fdc35b9b2b7a3b5a328be2e1dd0f0a3fe
Bryan Larsen <bryan@larsen.st>
2012-11-27 12:42:18 -0500

    enumerable monkey patch: fix for crash when used outside Rails

--- hobo/lib/hobo/extensions/enumerable.rb
+++ hobo/lib/hobo/extensions/enumerable.rb
@@ -1,10 +1,12 @@
 module Enumerable
   def group_by_with_metadata(&block)
     r=group_by_without_metadata(&block)
-    r.each do |k,v|
-      v.origin = origin
-      v.origin_attribute = origin_attribute
-      v.member_class = member_class
+    if respond_to?(:origin)
+      r.each do |k,v|
+        v.origin = origin
+        v.origin_attribute = origin_attribute
+        v.member_class = member_class
+      end
     end
     r
   end
