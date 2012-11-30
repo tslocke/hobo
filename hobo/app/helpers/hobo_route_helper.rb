64e99d6749a1a6af1219d64beb02baf64cd2da57
Bryan Larsen <bryan@larsen.st>
2012-11-30 13:37:07 -0500

    issue #9: previous fix caused problems when relative_url_root wasn't empty

--- hobo/app/helpers/hobo_route_helper.rb
+++ hobo/app/helpers/hobo_route_helper.rb
@@ -123,11 +123,14 @@ module HoboRouteHelper
       poly = [options[:subsite]] + poly if options[:subsite]
 
       begin
-        url = polymorphic_path(poly, params)
+        base_url = url = polymorphic_path(poly, params)
         # validate URL, since polymorphic URL may return a URL for a
         # different method
         # use starts_with because recognize path may return new_for_owner, for example
-        recognized_params = Rails.application.routes.recognize_path(url, {:method => options[:method]})
+        if Rails.application.config.action_controller.relative_url_root
+          base_url = url.gsub(/^#{Rails.application.config.action_controller.relative_url_root}/, "")
+        end
+        recognized_params = Rails.application.routes.recognize_path(base_url, {:method => options[:method]})
         if recognized_params[:action] == action.to_s
           url
         else
