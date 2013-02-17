0e0a14e5b83618c5e3527884bf367f9e156061d9
Bryan Larsen <bryan@larsen.st>
2013-02-16 21:49:10 -0500

    update hobo_field's activerecord hook for Rails 4

--- hobo_fields/lib/hobo_fields/extensions/active_record/attribute_methods.rb
+++ hobo_fields/lib/hobo_fields/extensions/active_record/attribute_methods.rb
@@ -1,4 +1,21 @@
 ActiveRecord::Base.class_eval do
+  def read_attribute_with_hobo(attr_name)
+    name = attr_name.to_s
+    if self.class.can_wrap_with_hobo_type?(name)
+      attr_name = attr_name.to_sym
+      val = read_attribute_without_hobo(name)
+      wrapper_type = self.class.attr_type(attr_name)
+      if HoboFields.can_wrap?(wrapper_type, val)
+        wrapper_type.new(val)
+      else
+        val
+      end
+    else
+      read_attribute_without_hobo(name)
+    end
+  end
+  alias_method_chain :read_attribute, :hobo
+
   class << self
 
     def can_wrap_with_hobo_type?(attr_name)
@@ -8,52 +25,6 @@ ActiveRecord::Base.class_eval do
       else
         false
       end
-    end
-
-    # Rails 3.2+
-    def internal_attribute_access_code(attr_name, cast_code)
-      access_code = "(v=@attributes[attr_name]) && #{cast_code}"
-
-      unless attr_name == primary_key
-        access_code.insert(0, "missing_attribute(attr_name, caller) unless @attributes.has_key?(attr_name); ")
-      end
-
-      # This is the Hobo hook - add a type wrapper around the field
-      # value if we have a special type defined
-      if can_wrap_with_hobo_type?(attr_name)
-        access_code = "val = begin; #{access_code}; end; wrapper_type = self.class.attr_type(:#{attr_name}); " +
-            "if HoboFields.can_wrap?(wrapper_type, val); wrapper_type.new(val); else; val; end"
-      end
-
-      if cache_attribute?(attr_name)
-        access_code = "@attributes_cache['#{attr_name}'] ||= begin; #{access_code}; end;"
-      end
-
-      "attr_name = '#{attr_name}'; #{access_code}"
-    end
-
-    # Rails 3.1 or earlier
-    # Define an attribute reader method.  Cope with nil column.
-    def define_read_method(symbol, attr_name, column)
-      cast_code = column.type_cast_code('v') if column
-      access_code = cast_code ? "(v=@attributes['#{attr_name}']) && #{cast_code}" : "@attributes['#{attr_name}']"
-
-      unless attr_name.to_s == self.primary_key.to_s
-        access_code = access_code.insert(0, "missing_attribute('#{attr_name}', caller) unless @attributes.has_key?('#{attr_name}'); ")
-      end
-
-      # This is the Hobo hook - add a type wrapper around the field
-      # value if we have a special type defined
-      if can_wrap_with_hobo_type?(symbol)
-        access_code = "val = begin; #{access_code}; end; wrapper_type = self.class.attr_type(:#{attr_name}); " +
-            "if HoboFields.can_wrap?(wrapper_type, val); wrapper_type.new(val); else; val; end"
-      end
-
-      if cache_attribute?(attr_name)
-        access_code = "@attributes_cache['#{attr_name}'] ||= begin; #{access_code}; end;"
-      end
-
-      generated_attribute_methods.module_eval("def #{symbol}; #{access_code}; end", __FILE__, __LINE__)
     end
 
     def define_method_attribute=(attr_name)
