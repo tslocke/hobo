d678d77850182aa0aabc2d7e69d2f11ebc145932
Bryan Larsen <bryan@larsen.st>
2013-02-24 10:18:50 -0500

    remove monkey patches for Chronic; not supported by Ruby 2.0.0.

--- hobo_support/lib/hobo_support.rb
+++ hobo_support/lib/hobo_support.rb
@@ -2,7 +2,6 @@ require 'active_support'
 require 'active_support/core_ext'
 require 'active_support/dependencies'
 
-require "hobo_support/fixes/chronic"
 require "hobo_support/fixes/pp"
 require "hobo_support/fixes/module"
 require 'hobo_support/blankslate'
