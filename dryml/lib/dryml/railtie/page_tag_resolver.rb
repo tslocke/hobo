2cd7dc9ae2dbc01af967e676bd165a0d084b9b57
Ignacio Huerta <ignacio@ihuerta.net>
2013-10-19 11:10:12 +0200

    Fix virtual path issue with automatic Hobo templates

--- dryml/lib/dryml/railtie/page_tag_resolver.rb
+++ dryml/lib/dryml/railtie/page_tag_resolver.rb
@@ -10,6 +10,7 @@ module Dryml
       def find_templates(name, prefix, partial, details)
         tag_name = @controller.dryml_fallback_tag || name.dasherize + '-page'
         method_name = tag_name.to_s.gsub('-', '_')
+        details[:virtual_path] = "#{prefix}/#{name}"
         if Dryml.empty_page_renderer(@controller.view_context).respond_to?(method_name)
           [ActionView::Template.new('', Dryml.page_tag_identifier(@controller.controller_path, tag_name),
                                     Dryml::Railtie::TemplateHandler, details)]
